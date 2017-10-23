906953cc707be37956b5328f6ee9db9fe637d673
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-10-23 13:42:57 +0200

    link to alex Run2016 dataset

--- ml/create_training_dataset.sh
+++ ml/create_training_dataset.sh
@@ -6,7 +6,7 @@ mkdir -p ml/${CHANNEL}
 
 python ml/write_dataset_config.py \
     --channel ${CHANNEL} \
-    --base-path /storage/jbod/wunsch/htt_2017-06-21_eleScale \
+    --base-path /storage/jbod/wunsch/Run2Analysis_alex \
     --database /portal/ekpbms3/home/wunsch/CMSSW_7_4_7/src/Kappa/Skimming/data/datasets.json \
     --output-path $PWD/ml/mt \
     --output-filename ${CHANNEL}_training_dataset.root \
--- ml/run_application.sh
+++ ml/run_application.sh
@@ -3,7 +3,7 @@
 CHANNEL=$1
 
 python ml/write_application_filelist.py \
-    --directory /storage/jbod/wunsch/htt_2017-06-21_eleScale_classified \
+    --directory /storage/jbod/wunsch/Run2Analysis_alex_classified \
     --database /portal/ekpbms3/home/wunsch/CMSSW_7_4_7/src/Kappa/Skimming/data/datasets.json \
     --channel ${CHANNEL} \
     --output ml/${CHANNEL}/application_filelist.yaml
