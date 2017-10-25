5517708433a53db091e92863d3ecaae2a01a5a57
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-10-25 16:25:17 +0200

    add new systematics to datacard production

--- datacards/produce_datacard.py
+++ datacards/produce_datacard.py
@@ -40,41 +40,44 @@ def main(args):
     db = DatacardBuilder(args.shapes)
 
     # Register observations, signals and backgrounds
-    categories = ["mt_Htt", "mt_Ztt", "mt_Zll", "mt_WJets", "mt_tt", "mt_QCD"]
-    category_pairs = db.make_pairs(categories)
+    mt_categories = [
+        "mt_Htt", "mt_Ztt", "mt_Zll", "mt_WJets", "mt_tt", "mt_QCD"
+    ]
+    mt_category_pairs = db.make_pairs(mt_categories)
 
-    db.add_observation("125", "smhtt", "Run2016", "mt", category_pairs)
+    db.add_observation("125", "smhtt", "Run2016", "mt", mt_category_pairs)
 
     signals = ["Htt"]
-    db.add_signals("125", "smhtt", "Run2016", "mt", signals, category_pairs)
+    db.add_signals("125", "smhtt", "Run2016", "mt", signals, mt_category_pairs)
 
     backgrounds = ["Ztt", "Zll", "WJets", "VV", "tt", "QCD"]
     db.add_backgrounds("125", "smhtt", "Run2016", "mt", backgrounds,
-                       category_pairs)
+                       mt_category_pairs)
 
     # Add shapes systematics
-    db.add_shape_systematic("CMS_scale_t_3prong0pi0_13TeV", 1.0,
+    db.add_shape_systematic("CMS_scale_t_3prong0pi0", 1.0, "mt",
                             ["Htt", "Ztt"])
-    db.add_shape_systematic("CMS_scale_t_1prong0pi0_13TeV", 1.0,
+    db.add_shape_systematic("CMS_scale_t_1prong0pi0", 1.0, "mt",
                             ["Htt", "Ztt"])
-    db.add_shape_systematic("CMS_scale_t_1prong1pi0_13TeV", 1.0,
+    db.add_shape_systematic("CMS_scale_t_1prong1pi0", 1.0, "mt",
                             ["Htt", "Ztt"])
-    db.add_shape_systematic("CMS_htt_dyShape_13TeV", 1.0, ["Ztt", "Zll"])
+    db.add_shape_systematic("CMS_htt_dyShape", 1.0, "mt", ["Ztt", "Zll"])
 
     # Add normalization systematics
-    db.add_normalization_systematic("lumi", signals + backgrounds, 1.025)
-    db.add_normalization_systematic("trigger_efficiency",
-                                    signals + backgrounds, 1.02)
-    db.add_normalization_systematic("muon_efficiency", signals + backgrounds,
-                                    1.02)
-    db.add_normalization_systematic("tau_efficiency", signals + backgrounds,
-                                    1.02)
-    db.add_normalization_systematic("norm_Ztt", "Ztt", 1.04)
-    db.add_normalization_systematic("norm_Zll", "Zll", 1.04)
-    db.add_normalization_systematic("norm_tt", "tt", 1.06)
-    db.add_normalization_systematic("norm_VV", "VV", 1.05)
-    db.add_normalization_systematic("norm_WJets", "WJets", 1.04)
-    db.add_normalization_systematic("QCD_extrapolation", "QCD", 1.06)
+    db.add_normalization_systematic("lumi_13TeV", 1.026, "mt",
+                                    signals + backgrounds)
+    db.add_normalization_systematic("CMS_eff_m", 1.02, "mt",
+                                    signals + backgrounds)
+    db.add_normalization_systematic("CMS_eff_trigger_mt", 1.02, "mt",
+                                    signals + backgrounds)
+    db.add_normalization_systematic("CMS_Extrap_SSOS_mt", 1.20, "mt", "QCD")
+    db.add_normalization_systematic("CMS_htt_wjXsec", 1.04, "mt", "WJets")
+    db.add_normalization_systematic("CMS_htt_vvXsec", 1.06, "mt", "VV")
+    # TODO: tt and z normalizations
+    """
+    db.add_normalization_systematic("CMS_htt_ttXsec", 1.00, "mt", "tt")
+    db.add_normalization_systematic("CMS_htt_zXsec", 1.00, "mt", ["Ztt", "Zll"])
+    """
 
     # Extract shapes
     db.extract_shapes("mt", "smhtt", "Run2016", "mt_keras2_max_score")
