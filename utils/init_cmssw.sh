5318e48ba9463b17a4c0822f6daad1c29b21cdeb
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-02 13:55:45 +0100

    add HarryPlotter to init CMSSW script

--- utils/init_cmssw.sh
+++ utils/init_cmssw.sh
@@ -10,4 +10,7 @@ cd CMSSW_7_4_7/src
 git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit -b 74x-root6
 git clone https://github.com/cms-analysis/CombineHarvester.git CombineHarvester -b analysis-HIG-16-006-freeze-080416
 
+# NOTE: Remove this as soon as HarryPlotter is replaced.
+git clone https://github.com/KIT-CMS/KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau -b master
+
 scramv1 b -j 24
