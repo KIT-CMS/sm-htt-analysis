50725cce606af6f741e52d14b0ca5b0902953784
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-05 14:49:46 +0100

    reenable mt control plots

--- plotting/plot_control.sh
+++ plotting/plot_control.sh
@@ -3,4 +3,4 @@
 source utils/setup_cmssw.sh
 
 ./plotting/plot_control.py -v et_keras20_max_score -c et_ZTT et_ZLL et_W et_TT et_QCD --scale-signal 50 --channel et
-#./plotting/plot_control.py -v mt_keras20_max_score -c mt_ZTT mt_ZLL mt_W mt_TT mt_QCD --scale-signal 50 --channel mt
+./plotting/plot_control.py -v mt_keras20_max_score -c mt_ZTT mt_ZLL mt_W mt_TT mt_QCD --scale-signal 50 --channel mt
