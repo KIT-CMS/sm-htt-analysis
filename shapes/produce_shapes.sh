eced33ff494c81bb3a9c238d2b8d60c26f97411a
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-06 10:50:13 +0100

    revert binning

--- shapes/produce_shapes.sh
+++ shapes/produce_shapes.sh
@@ -10,7 +10,7 @@ source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
 
 # Calculate binning from data distributions
-# NOTE: Binning is committed in repository.
+# NOTE: Binning is now committed in repository.
 #./shapes/calculate_binning.sh $ARTUS_OUTPUTS $KAPPA_DATABASE $ET_TRAINING $MT_TRAINING
 
 # Produce shapes
