115a3a4772a53dbf0476db8d4b26dc4d3aec05f8
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2017-11-07 18:22:22 +0100

    fixed mt label in postfit plots

--- plotting/plot_shapes.sh
+++ plotting/plot_shapes.sh
@@ -10,4 +10,4 @@ source utils/setup_cmssw.sh
 # mt
 ./plotting/plot_shapes.py -i datacard_shapes_prefit.root -f mt_ZTT_prefit mt_ZLL_prefit mt_QCD_prefit mt_W_prefit mt_TT_prefit mt_HTT_prefit --title "#mu#tau_{h}"
 
-./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -f mt_ZTT_postfit mt_ZLL_postfit mt_QCD_postfit mt_W_postfit mt_TT_postfit mt_HTT_postfit --title "mu#tau_{h}"
+./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -f mt_ZTT_postfit mt_ZLL_postfit mt_QCD_postfit mt_W_postfit mt_TT_postfit mt_HTT_postfit --title "#mu#tau_{h}"
