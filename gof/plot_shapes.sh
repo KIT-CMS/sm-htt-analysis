502aa7e869689bdd035ac0a71ebf3ddb29dcf75f
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-13 12:43:02 +0100

    remove unused postfit plots from gof jobs

--- gof/plot_shapes.sh
+++ gof/plot_shapes.sh
@@ -3,9 +3,7 @@
 CHANNEL=$1
 VARIABLE=$2
 
-./combine/prefit_postfit_shapes.sh
-
 source utils/setup_cmssw.sh
 
+PostFitShapes -m 125 -d datacard.txt -o datacard_shapes_prefit.root
 ./plotting/plot_shapes.py -i datacard_shapes_prefit.root -f ${CHANNEL}_${VARIABLE}_prefit
-./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -f ${CHANNEL}_${VARIABLE}_postfit
