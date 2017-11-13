4bca5ce4c8dd1a5055fa64f6503dd4f3027a4fd4
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-13 17:10:58 +0100

    nice up gof plots

--- gof/plot_gof.py
+++ gof/plot_gof.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 import logging
-logger = logging.getLogger("keras_confusion_matrix")
+logger = logging.getLogger("plot_gof")
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
@@ -27,12 +27,17 @@ def parse_arguments():
         "variables", help="Considered variables in goodness of fit test")
     parser.add_argument(
         "path", help="Path to directory with goodness of fit results")
+    parser.add_argument(
+        "--channels",
+        default=["et", "mt"],
+        nargs='+',
+        help="Select channels to be plotted")
     return parser.parse_args()
 
 
 def plot_1d(variables, results, filename):
     labels = [v for v in variables if v in results]
-    plt.figure(figsize=(len(labels) * 1.0, 7.0))
+    plt.figure(figsize=(len(labels) * 1.0, 5.0))
     y = [results[v] for v in labels]
     x = range(len(y))
     plt.plot(x, y, '+', mew=5, ms=20)
@@ -41,31 +46,31 @@ def plot_1d(variables, results, filename):
     plt.xticks(x, labels, rotation='vertical')
     plt.axhline(y=0.05, linewidth=3, color='r')
     plt.ylabel('Saturated goodness of fit p-value', labelpad=20)
+    ax = plt.gca()
+    ax.xaxis.grid()
     plt.savefig(filename, bbox_inches="tight")
 
 
-def search_results_1d(path, variables):
+def search_results_1d(path, channels, variables):
     results = {}
-    for dir_ in os.listdir(path):
-        filename = os.path.join(path, dir_, "gof.json")
-        if not os.path.exists(filename):
-            continue
+    missing = {}
+    for channel in channels:
+        results[channel] = {}
+        missing[channel] = []
+        for variable in variables:
+            filename = os.path.join(path, "{}_{}".format(channel, variable),
+                                    "gof.json")
+            if not os.path.exists(filename):
+                missing[channel].append(variable)
+                continue
+            logger.debug(
+                "Found goodness of fit result for variable %s in channel %s.",
+                variable, channel)
 
-        channel = dir_[0:2]
-        variable = dir_[3:]
-        if not variable in variables:
-            continue
-        logger.debug(
-            "Found goodness of fit result for variable %s in channel %s.",
-            variable, channel)
+            p_value = json.load(open(filename))
+            results[channel][variable] = p_value["125.0"]["p"]
 
-        p_value = json.load(open(filename))
-
-        if not channel in results:
-            results[channel] = {}
-        results[channel][variable] = p_value["125.0"]["p"]
-
-    return results
+    return missing, results
 
 
 def main(args):
@@ -76,9 +81,19 @@ def main(args):
         logger.fatal("Path %s does not exist.")
         raise Exception
 
+    # Plot 1D gof results
     variables = yaml.load(open(args.variables))["variables"]
-    results_1d = search_results_1d(args.path, variables)
-    for channel in results_1d:
+    missing_1d, results_1d = search_results_1d(args.path, args.channels,
+                                               variables)
+    for channel in args.channels:
+        logger.debug("Missing variables for 1D plots in channel %s.", channel)
+        for variable in missing_1d[channel]:
+            print("{} {}".format(channel, variable))
+
+    for channel in args.channels:
+        if results_1d[channel] == {}:
+            logger.warning("No results found for channel %s.", channel)
+            continue
         plot_1d(variables, results_1d[channel],
                 "{}_gof_1d.png".format(channel))
 
