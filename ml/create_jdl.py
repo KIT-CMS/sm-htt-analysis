6ca64ecf192e4ab28967940ca8b3a317c6c2e270
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-11-22 18:13:09 +0100

    add files to send jobs with full analysis to local batch system

---
+++ ml/create_jdl.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from classes.JDLCreator import JDLCreator
+import yaml
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output", type=str, help="Output path")
+    parser.add_argument("--channel", required=True, type=str, help="Channel")
+    parser.add_argument(
+        "--modifiers",
+        required=True,
+        type=str,
+        nargs="+",
+        help="Modifier values")
+    return parser.parse_args()
+
+
+def main(args):
+    jobs = JDLCreator("ekpsupermachines")
+
+    jobs.executable = "ml/job.sh"
+    jobs.wall_time = 10 * 60 * 60
+    jobs.memory = 2048
+    jobs.accounting_group = "cms.higgs"
+    jobs.image = "stwunsch/slc6-condocker:smhtt"
+
+    # Build list of arguments
+    arguments = []
+    for modifier in args.modifiers:
+        arguments.append("{} {}".format(args.channel, modifier))
+    jobs.arguments = arguments
+
+    # The job requires lots of CPU resources
+    # NOTE: This selects the sg machines.
+    jobs.requirements = "(Target.ProvidesCPU == True) && (Target.ProvidesEKPResources == True)"
+    jobs.job_folder = args.output
+    jobs.WriteJDL()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
---
+++ ml/job.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "### Begin of job"
+
+CHANNEL=$1
+echo "Channel:" $CHANNEL
+
+MODIFIER=$2
+echo "Modifier:" $MODIFIER
+
+BASE_PATH=/portal/ekpbms1/home/wunsch/sm-htt-analysis-ml
+echo "Base repository path:" $BASE_PATH
+
+OUTPUT_PATH=/storage/c/wunsch/trainings
+OUTPUT_DIR=${CHANNEL}_keras_qqH${MODIFIER}
+echo "Output: ${OUTPUT_PATH}/${OUTPUT_DIR}"
+
+echo "Hostname:" `hostname`
+
+echo "How am I?" `id`
+
+echo "Where am I?" `pwd`
+
+echo "### Start working"
+
+cp -r $BASE_PATH src
+cd src
+
+# Clean-up and set correct CMSSW path
+./utils/clean.sh
+sed -i "s%CMSSW_7_4_7%${BASE_PATH}/CMSSW_7_4_7%g" utils/setup_cmssw.sh
+
+# Training
+sed -i "s%MODIFIER%${MODIFIER}%g" ml/${CHANNEL}_training_config.yaml
+./ml/run_training.sh $CHANNEL
+./ml/run_testing.sh $CHANNEL
+
+# Application
+sed -i "s%${CHANNEL}_keras%${OUTPUT_DIR}%g" ml/${CHANNEL}_keras_application_config.yaml
+./ml/run_application.sh $CHANNEL
+
+# Combine
+sed -i "s%${CHANNEL}_keras%${OUTPUT_DIR}%g" utils/setup_samples.sh
+sed -i "s%default=32%default=12%g" shapes/produce_shapes.py
+./run_analysis $CHANNEL
+
+# Publish results
+./utils/publish_analysis.sh ${OUTPUT_PATH}/${OUTPUT_DIR}
+
+echo "### End of job"
