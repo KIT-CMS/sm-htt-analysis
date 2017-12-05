ee55a1e564eff849a2952680f0bc63121122276b
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-12-05 11:14:03 +0100

    point to new training path

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -2,8 +2,8 @@
 
 ARTUS_OUTPUTS=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2017-12-02/merged
 
-ARTUS_FRIENDS_ET=/storage/c/wunsch/Artus_2017-11-17/et_keras
-ARTUS_FRIENDS_MT=/storage/c/wunsch/Artus_2017-11-17/mt_keras
-ARTUS_FRIENDS_TT=/storage/c/wunsch/Artus_2017-11-17/tt_keras
+ARTUS_FRIENDS_ET=/storage/c/wunsch/Artus_2017-12-02/et_keras
+ARTUS_FRIENDS_MT=/storage/c/wunsch/Artus_2017-12-02/mt_keras
+ARTUS_FRIENDS_TT=/storage/c/wunsch/Artus_2017-12-02/tt_keras
 
 KAPPA_DATABASE=/storage/c/wunsch/kappa_database/datasets_2017-12-05.json
