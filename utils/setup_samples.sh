b1eda1aacd3c089796f7eaae60b09c7678c23904
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-12-07 16:26:54 +0100

    new ml setup with splitted models for different channels

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -2,8 +2,8 @@
 
 ARTUS_OUTPUTS=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2017-12-02/merged
 
-ARTUS_FRIENDS_ET=/storage/c/wunsch/Artus_2017-12-02/et_keras
-ARTUS_FRIENDS_MT=/storage/c/wunsch/Artus_2017-12-02/mt_keras
-ARTUS_FRIENDS_TT=/storage/c/wunsch/Artus_2017-12-02/tt_keras
+ARTUS_FRIENDS_ET=/storage/c/wunsch/Artus_2017-12-02/et_keras_1
+ARTUS_FRIENDS_MT=/storage/c/wunsch/Artus_2017-12-02/mt_keras_1
+ARTUS_FRIENDS_TT=/storage/c/wunsch/Artus_2017-12-02/tt_keras_1
 
 KAPPA_DATABASE=/storage/c/wunsch/kappa_database/datasets_2017-12-05.json
