43e0c19d478d5d9257ad9afc39d96789e13d1aa4
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-12-22 15:21:43 +0100

    push CVMFS SFT software stack to LCG92

--- utils/setup_cvmfs_sft.sh
+++ utils/setup_cvmfs_sft.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_91/x86_64-slc6-gcc62-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-slc6-gcc62-opt/setup.sh
 
 export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
