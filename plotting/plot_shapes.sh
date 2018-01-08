90312d56ebdc3ddf5698013a4d64f761d54414ea
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-01-08 13:19:29 +0100

    make directory for plotting if not existent

--- plotting/plot_shapes.sh
+++ plotting/plot_shapes.sh
@@ -3,6 +3,7 @@
 source utils/setup_cmssw.sh
 source utils/setup_python.sh
 
+mkdir -p plots
 ./plotting/plot_shapes.py -i datacard_shapes_prefit.root -c $@
 ./plotting/plot_shapes.py -i datacard_shapes_prefit.root --png -c $@
 ./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -c $@
