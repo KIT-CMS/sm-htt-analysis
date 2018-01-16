d9191c57aa95f481b49d6fa84b0745bbbb5e7bd3
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-01-16 17:24:22 +0100

    add cvmfs setup which sources on deepthought the correct software stack

--- utils/setup_cvmfs_sft.sh
+++ utils/setup_cvmfs_sft.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-slc6-gcc62-opt/setup.sh
+LCG_RELEASE=92
+
+if uname -a | grep ekpdeepthought
+then
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-ubuntu1604-gcc54-dbg/setup.sh
+else
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-slc6-gcc62-opt/setup.sh
+fi
 
 export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
