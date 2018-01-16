a8c1427210d236dcfbd05f1611726870249bc575
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-01-16 17:26:24 +0100

    add cuda gpu masking in training procedure

--- ml/run_training.sh
+++ ml/run_training.sh
@@ -7,6 +7,7 @@ source utils/setup_cvmfs_sft.sh
 export KERAS_BACKEND=theano
 export OMP_NUM_THREADS=12
 export THEANO_FLAGS=gcc.cxxflags=-march=corei7
+export CUDA_VISIBLE_DEVICES=3
 
 mkdir -p ml/${CHANNEL}
 
