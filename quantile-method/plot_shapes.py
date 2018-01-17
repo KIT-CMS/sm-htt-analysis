87b8caa9e64e6a553788c1f8886939b1fb421a23
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2018-01-17 18:43:48 +0100

    update impact-parameter shape production and plotting

--- quantile-method/plot_shapes.py
+++ quantile-method/plot_shapes.py
@@ -69,6 +69,8 @@ def setup_logging(output_file, level=logging.DEBUG):
 
 def main(args):
     channel_categories = {
+        "ee": ["m_vis", "d0_e", "dZ_e"],
+        "mm": ["m_vis", "d0_m", "dZ_m"],
         "et": ["d0_te", "dZ_te", "d0_t", "dZ_t"],
         "mt": ["d0_tm", "dZ_tm", "d0_t", "dZ_t", "d0_f", "dZ_f", "d0_tt", "dZ_tt"],
         "tt": ["d0_t2", "dZ_t2"]
@@ -82,6 +84,10 @@ def main(args):
         "tt": "#tau_{h}#tau_{h}"
     }
     category_dict = {
+        "d0_e": "#rightarrowe",
+        "dZ_e": "#rightarrowe",
+        "d0_m": "#rightarrowm",
+        "dZ_m": "#rightarrowm",
         "d0_te": "#rightarrow#tau#rightarrowe",
         "dZ_te": "#rightarrow#tau#rightarrowe",
         "d0_tm": "#rightarrow#tau#rightarrowm",
@@ -93,9 +99,14 @@ def main(args):
         "d0_f":  "fake#rightarrow#tau_{h}",
         "dZ_f":  "fake#rightarrow#tau_{h}",
         "d0_tt": "fake#rightarrow#tau_{h}",
-        "dZ_tt": "fake#rightarrow#tau_{h}"
+        "dZ_tt": "fake#rightarrow#tau_{h}",
+        "m_vis": "inclusive"
     }
     variable_dict = {
+        "d0_e": "d0_1",
+        "dZ_e": "dZ_1",
+        "d0_m": "d0_1",
+        "dZ_m": "dZ_1",
         "d0_te": "d0_1",
         "dZ_te": "dZ_1",
         "d0_tm": "d0_1",
@@ -107,12 +118,30 @@ def main(args):
         "d0_f":  "d0_2",
         "dZ_f":  "dZ_2",
         "d0_tt": "d0_2",
-        "dZ_tt": "dZ_2"
+        "dZ_tt": "dZ_2",
+        "m_vis": "m_vis"
+    }
+    split_dict = {
+        "ee": 10000000,
+        "em": 10000000,
+        "et": 10,
+        "mm": 100000000,
+        "mt": 10,
+        "tt": 10
     }
 
-    MC_processes = ["HTT", "EWK", "QCD", "VV", "W", "TTT", "TTJ", "ZJ", "ZL", "ZTT"]
-    legend_MC_processes = copy.deepcopy(MC_processes)
-    legend_MC_processes.reverse()
+    MC_processes = {
+        "ee": ["HTT", "EWK", "QCD", "VV", "W", "TT", "ZJ", "ZTT", "ZL"],
+        "em": ["HTT", "EWK", "QCD", "VV", "W", "TT", "ZJ", "ZTT", "ZL"],
+        "et": ["HTT", "EWK", "QCD", "VV", "W", "TTT", "TTJ", "ZJ", "ZTT", "ZL"],
+        "mm": ["HTT", "EWK", "QCD", "VV", "W", "TT", "ZJ", "ZTT", "ZL"],
+        "mt": ["HTT", "EWK", "QCD", "VV", "W", "TTT", "TTJ", "ZJ", "ZTT", "ZL"],
+        "tt": ["HTT", "EWK", "QCD", "VV", "W", "TTT", "TTJ", "ZJ", "ZTT", "ZL"]
+    }
+    legend_MC_processes={}
+    for channel in args.channels:
+        legend_MC_processes[channel] = copy.deepcopy(MC_processes[channel])
+        legend_MC_processes[channel].reverse()
 
     rootfile = ROOT.TFile(args.input, "READ")
 
@@ -121,7 +150,8 @@ def main(args):
     for channel in args.channels:
         for category in channel_categories[channel]:
             plot = dd.Plot([0.5, [0.35, 0.33]], "ModTDR", r=0.04, l=0.14)
-            for process in MC_processes:
+            for process in MC_processes[channel]:
+                #print "#{channel}#{channel}_{category}#{process}#smhtt#Run2016#{var}#125#".format(channel=channel, category=category, process=process, var=variable_dict[category])
                 plot.add_hist(
                     rootfile.Get("#{channel}#{channel}_{category}#{process}#smhtt#Run2016#{var}#125#".format(channel=channel, category=category, process=process, var=variable_dict[category])), process, "MC_single")
                 plot.setGraphStyle(
@@ -136,12 +166,15 @@ def main(args):
                 "data_obs", "e0")
 
             plot.subplot(2).normalize(["MC", "data_obs"], "data_obs")
+            plot.subplot(0).normalizeByBinWidth()
+            plot.subplot(1).normalizeByBinWidth()
 
-            plot.create_stack(MC_processes, "stack")
+            plot.create_stack(MC_processes[channel], "stack")
 
-            plot.subplot(0).setYlims(10.0, 2 * plot.subplot(0).get_hist("MC").GetMaximum())
+            plot.subplot(0).setYlims(split_dict[channel], 2 * plot.subplot(0).get_hist("MC").GetMaximum())
             #plot.subplot(0).setYlims(10, 10000000)
-            plot.subplot(1).setYlims(0.1, 10)
+            plot.subplot(1).setYlims(1.0, split_dict[channel])
+            plot.subplot(2).setYlims(0.6, 1.4)
             plot.subplot(2).setXlabel(variable_dict[category])
             plot.subplot(0).setYlabel("N_{events}")
             plot.subplot(1).setYlabel("")
@@ -166,7 +199,12 @@ def main(args):
             # create legends
             suffix = ["", "_top"]
             plot.add_legend(width=0.48, height=0.15)
-            for process in legend_MC_processes:
+            
+            label_dict=styles.label_dict
+            label_dict["EWK"]="EWKZ"
+            label_dict["ZL"]=label_dict["ZL"].replace(" (l#rightarrow#tau_{h})", "")
+            label_dict["ZJ"]=label_dict["ZJ"].replace("#tau_{h}", "l")
+            for process in legend_MC_processes[channel]:
                 plot.legend(0).add_entry(0, process,
                                          styles.label_dict[process.replace(
                                              "EWK", "EWKZ")], 'f')
@@ -185,6 +223,22 @@ def main(args):
             prefix = "prefit" if "prefit" in args.input else "postfit" if "postfit" in args.input else "undefined"
             plot.save("quantile-method/%s_%s.%s" % (channel, category, 'png'
                                              if args.png else 'pdf'))
+            
+            # plot zoomed version
+            plot.setXlims(-0.02, 0.02)
+            plot.scaleYTitleSize(1.0)
+            plot.scaleYTitleOffset(1.0)
+            plot.subplot(1).Draw(["stack", "data_obs"])
+            plot.subplot(0).Draw(["stack", "data_obs"])
+            plot.subplot(2).Draw(["MC", "data_obs"])
+            plot.legend(0).Draw()
+            plot.DrawCMS()
+            plot.DrawLumi("35.9 fb^{-1} (13 TeV)")
+            plot.DrawChannelCategoryLabel("%s, %s" % (channel_dict[channel],
+                                                      category_dict[category]))
+            plot.save("quantile-method/zoom_%s_%s.%s" % (channel, category, 'png'
+                                             if args.png else 'pdf'))
+            
             plots.append(
                 plot
             )  # work around to have clean up seg faults only at the end of the script
--- quantile-method/produce_shapes.py
+++ quantile-method/produce_shapes.py
@@ -11,12 +11,13 @@ from shape_producer.process import Process
 from shape_producer.estimation_methods_2016 import *
 from shape_producer.estimation_methods import *
 from shape_producer.era import Run2016
-from shape_producer.channel import ETSM, MTSM, TTSM
+from shape_producer.channel import EESM, EMSM, MMSM, ETSM, MTSM, TTSM
 
 from itertools import product
 
 import argparse
 import yaml
+import copy
 
 import logging
 logger = logging.getLogger("")
@@ -79,6 +80,57 @@ def main(args):
     # Channels and processes
     # yapf: disable
     directory = args.directory
+    ee = EESM()
+    ee_processes = {
+        "data"  : Process("data_obs", DataEstimation  (era, directory, ee)),
+        "HTT"   : Process("HTT",      HTTEstimation   (era, directory, ee)),
+        "ggH"   : Process("ggH",      ggHEstimation   (era, directory, ee)),
+        "qqH"   : Process("qqH",      qqHEstimation   (era, directory, ee)),
+        "VH"    : Process("VH",       VHEstimation    (era, directory, ee)),
+        "ZTT"   : Process("ZTT",      ZTTEstimationLL (era, directory, ee)),
+        "ZL"    : Process("ZL",       ZLEstimationLL  (era, directory, ee)),
+        "ZJ"    : Process("ZJ",       ZJEstimationLL  (era, directory, ee)),
+        "W"     : Process("W",        WEstimation     (era, directory, ee)),
+        "TT"    : Process("TT",       TTEstimation    (era, directory, ee)),
+        "VV"    : Process("VV",       VVEstimation    (era, directory, ee)),
+        "EWK"   : Process("EWK",      EWKEstimation   (era, directory, ee))
+        }
+    ee_processes["QCD"] = Process("QCD", QCDEstimationET(era, directory, ee, [ee_processes[process] for process in ["ZTT", "ZJ", "ZL", "W", "TT", "VV", "EWK"]], ee_processes["data"], extrapolation_factor=1.0))
+    ee_processes["MC"] = Process("MC", SumUpEstimationMethod("MC", "nominal", era, directory, ee, [ee_processes[process] for process in ["ZTT", "ZJ", "ZL", "W", "TT", "VV", "EWK", "QCD", "HTT"]]))
+    em = EMSM()
+    em_processes = {
+        "data"  : Process("data_obs", DataEstimation  (era, directory, em)),
+        "HTT"   : Process("HTT",      HTTEstimation   (era, directory, em)),
+        "ggH"   : Process("ggH",      ggHEstimation   (era, directory, em)),
+        "qqH"   : Process("qqH",      qqHEstimation   (era, directory, em)),
+        "VH"    : Process("VH",       VHEstimation    (era, directory, em)),
+        "ZTT"   : Process("ZTT",      ZTTEstimationLL (era, directory, em)),
+        "ZL"    : Process("ZL",       ZLEstimationLL  (era, directory, em)),
+        "ZJ"    : Process("ZJ",       ZJEstimationLL  (era, directory, em)),
+        "W"     : Process("W",        WEstimation     (era, directory, em)),
+        "TT"    : Process("TT",       TTEstimation    (era, directory, em)),
+        "VV"    : Process("VV",       VVEstimation    (era, directory, em)),
+        "EWK"   : Process("EWK",      EWKEstimation   (era, directory, em))
+        }
+    em_processes["QCD"] = Process("QCD", QCDEstimationMT(era, directory, em, [em_processes[process] for process in ["ZTT", "ZJ", "ZL", "W", "TT", "VV", "EWK"]], em_processes["data"], extrapolation_factor=1.0))
+    em_processes["MC"] = Process("MC", SumUpEstimationMethod("MC", "nominal", era, directory, em, [em_processes[process] for process in ["ZTT", "ZJ", "ZL", "W", "TT", "VV", "EWK", "QCD", "HTT"]]))
+    mm = MMSM()
+    mm_processes = {
+        "data"  : Process("data_obs", DataEstimation  (era, directory, mm)),
+        "HTT"   : Process("HTT",      HTTEstimation   (era, directory, mm)),
+        "ggH"   : Process("ggH",      ggHEstimation   (era, directory, mm)),
+        "qqH"   : Process("qqH",      qqHEstimation   (era, directory, mm)),
+        "VH"    : Process("VH",       VHEstimation    (era, directory, mm)),
+        "ZTT"   : Process("ZTT",      ZTTEstimationLL (era, directory, mm)),
+        "ZL"    : Process("ZL",       ZLEstimationLL  (era, directory, mm)),
+        "ZJ"    : Process("ZJ",       ZJEstimationLL  (era, directory, mm)),
+        "W"     : Process("W",        WEstimation     (era, directory, mm)),
+        "TT"    : Process("TT",       TTEstimation    (era, directory, mm)),
+        "VV"    : Process("VV",       VVEstimation    (era, directory, mm)),
+        "EWK"   : Process("EWK",      EWKEstimation   (era, directory, mm))
+        }
+    mm_processes["QCD"] = Process("QCD", QCDEstimationMT(era, directory, mm, [mm_processes[process] for process in ["ZTT", "ZJ", "ZL", "W", "TT", "VV", "EWK"]], mm_processes["data"], extrapolation_factor=1.0))
+    mm_processes["MC"] = Process("MC", SumUpEstimationMethod("MC", "nominal", era, directory, mm, [mm_processes[process] for process in ["ZTT", "ZJ", "ZL", "W", "TT", "VV", "EWK", "QCD", "HTT"]]))
     mt = MTSM()
     mt.cuts.remove("muon_iso")
     mt.cuts.remove("tau_iso")
@@ -142,15 +194,89 @@ def main(args):
     tt_processes["MC"] = Process("MC", SumUpEstimationMethod("MC", "nominal", era, directory, tt, [tt_processes[process] for process in ["ZTT", "ZJ", "ZL", "W", "TTT", "TTJ", "VV", "EWK", "QCD", "HTT"]]))
 
     # Variables and categories
-    binning_lepton = [-0.1, -0.08, -0.06, -0.04, -0.02, -0.015, -0.01, -0.008, -0.006, -0.004, -0.003, -0.002, -0.001, 0.0, 0.001, 0.002, 0.003, 0.004, 0.006, 0.008, 0.01, 0.015, 0.02, 0.04, 0.06, 0.08, 0.1]
+    #binning_lepton_0 = [-0.1, -0.08, -0.06, -0.04, -0.02, -0.015, -0.01, -0.008, -0.006, -0.004, -0.003, -0.002, -0.001, 0.0, 0.001, 0.002, 0.003, 0.004, 0.006, 0.008, 0.01, 0.015, 0.02, 0.04, 0.06, 0.08, 0.1]
+    #binning_lepton_Z = [-0.15, -0.1, -0.08, -0.06, -0.04, -0.02, -0.015, -0.01, -0.008, -0.006, -0.004, -0.003, -0.002, -0.001, 0.0, 0.001, 0.002, 0.003, 0.004, 0.006, 0.008, 0.01, 0.015, 0.02, 0.04, 0.06, 0.08, 0.1, 0.15]
+    #binning_lepton_0_raw = [-0.05, -0.04, -0.03, -0.02, -0.015, -0.0125, -0.01, -0.009, -0.008, -0.007, -0.006, -0.005, -0.0045, -0.004, -0.0035, -0.003, -0.0025, -0.002, -0.0015, -0.001, -0.0006, -0.0003, 0.0, 0.0003, 0.0006, 0.001, 0.0015, 0.002, 0.0025, 0.003, 0.0035, 0.004, 0.0045, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.0125, 0.015, 0.02, 0.03, 0.04, 0.05]
+    binning_lepton_Z_raw = [0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.001, 0.00125, 0.0015, 0.00175, 0.002, 0.00225, 0.0025, 0.00275, 0.003, 0.00325, 0.0035, 0.00375, 0.004, 0.0045, 0.005, 0.0055, 0.006, 0.0065, 0.007, 0.0075, 0.008, 0.0085, 0.009, 0.0095, 0.01, 0.011, 0.012, 0.013, 0.014, 0.015, 0.016, 0.018, 0.02, 0.0225, 0.025, 0.0275, 0.03, 0.035, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.15, 0.2]
+    binning_lepton_0_raw = [0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.001, 0.00125, 0.0015, 0.00175, 0.002, 0.00225, 0.0025, 0.00275, 0.003, 0.00325, 0.0035, 0.00375, 0.004, 0.00425, 0.0045, 0.00475, 0.005, 0.0055, 0.006, 0.0065, 0.007, 0.0075, 0.008, 0.0085, 0.009, 0.0095, 0.01, 0.012, 0.014, 0.017, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045]
+    
+    binning_lepton_0 = []
+    for i in range(len(binning_lepton_0_raw)):
+        binning_lepton_0.append(-1*binning_lepton_0_raw[-i-1])
+    binning_lepton_0 += [0.0] + binning_lepton_0_raw
+    binning_lepton_Z = []
+    for i in range(len(binning_lepton_Z_raw)):
+        binning_lepton_Z.append(-1*binning_lepton_Z_raw[-i-1])
+    binning_lepton_Z += [0.0] + binning_lepton_Z_raw
+    
     binning_tau = [-0.1, -0.08, -0.06, -0.04, -0.02, -0.015, -0.01, -0.008, -0.006, -0.004, -0.002, 0.0, 0.002, 0.004, 0.006, 0.008, 0.01, 0.015, 0.02, 0.04, 0.06, 0.08, 0.1]
-    d0_1 = Variable("d0_1", VariableBinning(binning_lepton))
+    d0_1 = Variable("d0_1", VariableBinning(binning_lepton_0))
     #d0_1 = Variable("m_vis", VariableBinning([50.+x*5. for x in range(21)]))
     d0_2 = Variable("d0_2", VariableBinning(binning_tau))
     #d0_2 = Variable("m_vis", VariableBinning([50.+x*5. for x in range(21)]))
-    dZ_1 = Variable("dZ_1", VariableBinning(binning_lepton))
+    dZ_1 = Variable("dZ_1", VariableBinning(binning_lepton_Z))
     dZ_2 = Variable("dZ_2", VariableBinning(binning_tau))
+    m_vis = Variable("m_vis", VariableBinning([50.+x*5. for x in range(21)]))
 
+    ee_categories = []
+    if "ee" in args.channels:
+        ee_categories.append(
+            Category(
+                "m_vis",
+                ee,
+                Cuts(),
+                variable=m_vis))
+        ee_categories.append(
+            Category(
+                "d0_e",
+                ee,
+                Cuts(
+                    Cut("m_vis>80 && m_vis<100", "Zpeak")
+                    ),
+                variable=d0_1))
+        ee_categories.append(
+            Category(
+                "dZ_e",
+                ee,
+                Cuts(
+                    Cut("m_vis>80 && m_vis<100", "Zpeak")
+                    ),
+                variable=dZ_1))
+    
+    em_categories = []
+    if "em" in args.channels:
+        em_categories.append(
+            Category(
+                "m_vis",
+                em,
+                Cuts(),
+                variable=m_vis))
+    
+    mm_categories = []
+    if "mm" in args.channels:
+        mm_categories.append(
+            Category(
+                "m_vis",
+                mm,
+                Cuts(),
+                variable=m_vis))
+        mm_categories.append(
+            Category(
+                "d0_m",
+                mm,
+                Cuts(
+                    Cut("m_vis>80 && m_vis<100", "Zpeak")
+                    ),
+                variable=d0_1))
+        mm_categories.append(
+            Category(
+                "dZ_m",
+                mm,
+                Cuts(
+                    Cut("m_vis>80 && m_vis<100", "Zpeak")
+                    ),
+                variable=dZ_1))
+    
     et_categories = []
     if "et" in args.channels:
         et_categories.append(
@@ -304,6 +430,26 @@ def main(args):
                 variable=dZ_2))
     # Nominal histograms
     # yapf: enable
+    if "ee" in args.channels:
+        for process, category in product(ee_processes.values(), ee_categories):
+            systematics.add(
+                Systematic(
+                    category=category,
+                    process=process,
+                    analysis="smhtt",
+                    era=era,
+                    variation=Nominal(),
+                    mass="125"))
+    if "mm" in args.channels:
+        for process, category in product(mm_processes.values(), mm_categories):
+            systematics.add(
+                Systematic(
+                    category=category,
+                    process=process,
+                    analysis="smhtt",
+                    era=era,
+                    variation=Nominal(),
+                    mass="125"))
     if "et" in args.channels:
         for process, category in product(et_processes.values(), et_categories):
             systematics.add(
