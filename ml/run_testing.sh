c5194811c5ef2e180ae85d398a2042b4951ab646
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-01-21 18:44:17 +0100

    add AMS score for testing ml models

--- ml/run_testing.sh
+++ ml/run_testing.sh
@@ -10,6 +10,14 @@ export THEANO_FLAGS=gcc.cxxflags=-march=corei7
 
 mkdir -p ml/${CHANNEL}
 
+# AMS score
+python htt-ml/testing/keras_ams_score.py \
+    ml/${CHANNEL}_training_config.yaml ml/${CHANNEL}_keras_testing_config.yaml 0
+
+python htt-ml/testing/keras_ams_score.py \
+    ml/${CHANNEL}_training_config.yaml ml/${CHANNEL}_keras_testing_config.yaml 1
+
+# Confusion matrices
 python htt-ml/testing/keras_confusion_matrix.py \
     ml/${CHANNEL}_training_config.yaml ml/${CHANNEL}_keras_testing_config.yaml 0
 
