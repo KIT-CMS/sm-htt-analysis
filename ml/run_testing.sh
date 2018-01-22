0cbc6835858b4284114c1547e149176b82eed4b3
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-01-22 15:04:37 +0100

    remove ams score from ml testing

--- ml/run_testing.sh
+++ ml/run_testing.sh
@@ -17,13 +17,6 @@ fi
 
 mkdir -p ml/${CHANNEL}
 
-# AMS score
-python htt-ml/testing/keras_ams_score.py \
-    ml/${CHANNEL}_training_config.yaml ml/${CHANNEL}_keras_testing_config.yaml 0
-
-python htt-ml/testing/keras_ams_score.py \
-    ml/${CHANNEL}_training_config.yaml ml/${CHANNEL}_keras_testing_config.yaml 1
-
 # Confusion matrices
 python htt-ml/testing/keras_confusion_matrix.py \
     ml/${CHANNEL}_training_config.yaml ml/${CHANNEL}_keras_testing_config.yaml 0
