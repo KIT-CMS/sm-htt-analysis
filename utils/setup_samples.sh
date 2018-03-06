a2df2ce458edb49d07e8d4d357358ec5d4dc1513
janekbechtel <janek.bechtel@student.kit.edu>
2018-03-06 10:16:14 +0100

    update file paths

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ARTUS_OUTPUTS=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2017-12-02/merged
+ARTUS_OUTPUTS=/storage/c/jbechtel/Artus_2017-03-06/
 
-ARTUS_FRIENDS_ET=/storage/c/jbechtel/Artus_2017-12-02/et_keras_emb
-ARTUS_FRIENDS_MT=/storage/c/jbechtel/Artus_2017-12-02/mt_keras_emb
-ARTUS_FRIENDS_TT=/storage/c/wunsch/Artus_2017-12-02/tt_keras_4
+ARTUS_FRIENDS_ET=/storage/c/jbechtel/Artus_2017-03-06/et_keras_emb/
+ARTUS_FRIENDS_MT=/storage/c/jbechtel/Artus_2017-03-06/mt_keras_emb/
+ARTUS_FRIENDS_TT=/storage/c/wunsch/Artus_2017-12-02/tt_keras_4/
 
 KAPPA_DATABASE=/storage/c/wunsch/kappa_database/datasets_2017-12-05.json
