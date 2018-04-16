35d1bc2a01223401df2a4bb5aad3f10b4c09d97d
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-04-16 15:36:35 +0200

    Prepare for Taylor analysis of neural networks

--- utils/setup_python.sh
+++ utils/setup_python.sh
@@ -3,3 +3,5 @@
 export PYTHONPATH=$PYTHONPATH:$PWD/shape-producer
 export PYTHONPATH=$PYTHONPATH:$PWD/datacard-producer
 export PYTHONPATH=$PYTHONPATH:$PWD/Dumbledraw
+export PYTHONPATH=$PYTHONPATH:$PWD/tensorflow_derivative
+export PYTHONPATH=$PYTHONPATH:$PWD/htt-ml
