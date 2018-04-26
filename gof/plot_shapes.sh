3b2a4aff00d3725d2d29c6e9be512973577e7bd7
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-04-26 13:47:45 +0200

    fix plotting for gof shapes

--- gof/plot_shapes.sh
+++ gof/plot_shapes.sh
@@ -7,7 +7,7 @@ source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
 
 mkdir -p plots
-./plotting/plot_shapes.py -i datacard_shapes_prefit.root -c $CHANNEL --x-label $VARIABLE --gof-variable $VARIABLE
-./plotting/plot_shapes.py -i datacard_shapes_prefit.root -c $CHANNEL --x-label $VARIABLE --gof-variable $VARIABLE --png
-./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -c $CHANNEL --x-label $VARIABLE --gof-variable $VARIABLE
-./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -c $CHANNEL --x-label $VARIABLE --gof-variable $VARIABLE --png
+./plotting/plot_shapes.py -i datacard_shapes_prefit.root -c $CHANNEL --gof-variable $VARIABLE
+./plotting/plot_shapes.py -i datacard_shapes_prefit.root -c $CHANNEL --gof-variable $VARIABLE --png
+./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -c $CHANNEL --gof-variable $VARIABLE
+./plotting/plot_shapes.py -i datacard_shapes_postfit_sb.root -c $CHANNEL --gof-variable $VARIABLE --png
