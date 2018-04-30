794765e355f4cd8409addf5c1b769c58e1a565c5
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-04-30 17:02:23 +0200

    fix application run-script

--- ml/run_application.sh
+++ ml/run_application.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-CHANNEL=$1
+ERA=$1
+CHANNEL=$2
 
 source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
-source utils/setup_samples.sh
+source utils/setup_samples.sh $ERA
 
 python ml/write_application_filelist.py \
     --directory $ARTUS_OUTPUTS \
     --database $KAPPA_DATABASE \
     --channel ${CHANNEL} \
-    --output ml/${CHANNEL}/application_filelist.yaml
+    --output ml/${ERA}_${CHANNEL}/application_filelist.yaml
 
 export KERAS_BACKEND=theano
 export OMP_NUM_THREADS=1
 export THEANO_FLAGS=gcc.cxxflags=-march=corei7
 
 python ml/run_application.py \
-    --dataset-config ml/${CHANNEL}/dataset_config.yaml \
-    --training-config ml/${CHANNEL}_training_config.yaml \
-    --application-config ml/${CHANNEL}_keras_application_config.yaml \
-    --filelist ml/${CHANNEL}/application_filelist.yaml \
+    --dataset-config ml/${ERA}_${CHANNEL}/dataset_config.yaml \
+    --training-config ml/${ERA}_${CHANNEL}_training.yaml \
+    --application-config ml/${ERA}_${CHANNEL}_application.yaml \
+    --filelist ml/${ERA}_${CHANNEL}/application_filelist.yaml \
     --num-processes 12
