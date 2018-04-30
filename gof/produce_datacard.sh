1bae1288fe3d5b1b658587f8580706c95abdb5e9
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-04-30 18:05:18 +0200

    fix production of datacards for gof tests

--- gof/produce_datacard.sh
+++ gof/produce_datacard.sh
@@ -7,4 +7,9 @@ VARIABLE=$3
 source utils/setup_cmssw.sh
 source utils/setup_python.sh
 
-python datacards/produce_datacard.py --era $ERA --channels $CHANNEL --gof $VARIABLE --use-data-for-observation
+python datacards/produce_datacard.py \
+    --era $ERA \
+    --channels $CHANNEL \
+    --gof $VARIABLE \
+    --shapes ${ERA}_shapes.root \
+    --use-data-for-observation
