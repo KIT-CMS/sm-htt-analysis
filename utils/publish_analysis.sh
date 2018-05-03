faa3dc7749ce5a5c969a7b79171458ad059c475c
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-05-03 07:24:52 +0200

    add impacts to published analysis

--- utils/publish_analysis.sh
+++ utils/publish_analysis.sh
@@ -3,27 +3,34 @@
 ERA=$1
 OUTPUT_PATH=$2
 
+# Category plots
 mkdir -p ${OUTPUT_PATH}
 cp plots/*.png ${OUTPUT_PATH}
 cp utils/index.php ${OUTPUT_PATH}
 
+# ML
 for CHANNEL in "et" "mt" "tt"
 do
     mkdir -p ${OUTPUT_PATH}/${ERA}_${CHANNEL}
+    cp utils/index.php ${OUTPUT_PATH}/${ERA}_${CHANNEL}
+
     cp ml/${ERA}_${CHANNEL}/*.png ${OUTPUT_PATH}/${ERA}_${CHANNEL}/
     cp ml/${ERA}_${CHANNEL}/*.txt ${OUTPUT_PATH}/${ERA}_${CHANNEL}/
-    cp utils/index.php ${OUTPUT_PATH}/${ERA}_${CHANNEL}
 done
 
+# Logs
 mkdir -p ${OUTPUT_PATH}/log
-cp *.log ${OUTPUT_PATH}/log
 cp utils/index.php ${OUTPUT_PATH}/log
+cp *.log ${OUTPUT_PATH}/log
 
+# Datacard
 mkdir -p ${OUTPUT_PATH}/datacard
+cp utils/index.php ${OUTPUT_PATH}/datacard
 cp *.txt ${OUTPUT_PATH}/datacard
 cp *.root ${OUTPUT_PATH}/datacard
-cp utils/index.php ${OUTPUT_PATH}/datacard
 
+# Pulls and impacts
 mkdir -p ${OUTPUT_PATH}/pulls
-cp *.html ${OUTPUT_PATH}/pulls
 cp utils/index.php ${OUTPUT_PATH}/pulls
+cp *.html ${OUTPUT_PATH}/pulls
+cp *impacts.pdf ${OUTPUT_PATH}/pulls
