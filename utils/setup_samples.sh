68e8b042b92bf3f8130e1cbc89ea7ec21ce96d55
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-05-26 09:36:37 +0200

    add new training with new background estimations

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -4,9 +4,9 @@ ERA=$1
 
 # Samples Run2016
 ARTUS_OUTPUTS_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-04-26/merged
-ARTUS_FRIENDS_ET_2016=/storage/c/wunsch/Artus_2018-04-26/et_keras_4
-ARTUS_FRIENDS_MT_2016=/storage/c/wunsch/Artus_2018-04-26/mt_keras_4
-ARTUS_FRIENDS_TT_2016=/storage/c/wunsch/Artus_2018-04-26/tt_keras_4
+ARTUS_FRIENDS_ET_2016=/storage/c/wunsch/Artus_2018-04-26/et_keras_5
+ARTUS_FRIENDS_MT_2016=/storage/c/wunsch/Artus_2018-04-26/mt_keras_5
+ARTUS_FRIENDS_TT_2016=/storage/c/wunsch/Artus_2018-04-26/tt_keras_5
 
 # Error-handling
 if [[ $ERA == *"2016"* ]]
