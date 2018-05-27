b32b89ae069c7f0ee9e2d8895f37cda1869cd4b1
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-05-27 20:09:06 +0200

    split production of shapes by channel because maximum number of open files is reached

--- shapes/produce_shapes.sh
+++ shapes/produce_shapes.sh
@@ -15,15 +15,24 @@ source utils/setup_samples.sh $ERA
 #./shapes/calculate_binning.sh tt
 
 # Produce shapes
-python shapes/produce_shapes.py \
-    --directory $ARTUS_OUTPUTS \
-    --et-friend-directory $ARTUS_FRIENDS_ET \
-    --mt-friend-directory $ARTUS_FRIENDS_MT \
-    --tt-friend-directory $ARTUS_FRIENDS_TT \
-    --datasets $KAPPA_DATABASE \
-    --binning $BINNING \
-    --channels $CHANNELS \
-    --era $ERA
+for CHANNEL in $CHANNELS
+do
+    python shapes/produce_shapes.py \
+        --directory $ARTUS_OUTPUTS \
+        --et-friend-directory $ARTUS_FRIENDS_ET \
+        --mt-friend-directory $ARTUS_FRIENDS_MT \
+        --tt-friend-directory $ARTUS_FRIENDS_TT \
+        --datasets $KAPPA_DATABASE \
+        --binning $BINNING \
+        --channels $CHANNEL \
+        --era $ERA \
+        --tag ${ERA}_${CHANNEL} \
+        --num-threads 8 &
+done
+
+wait
+
+hadd -f ${ERA}_shapes.root ${ERA}_*_shapes.root
 
 # Convert shapes to synced format
 python shapes/convert_to_synced_shapes.py ${ERA}_shapes.root .
