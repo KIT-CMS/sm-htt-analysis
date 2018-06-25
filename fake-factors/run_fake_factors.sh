69b2b0c5914857866e4d1af3592362b21b35d196
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2018-06-25 18:08:00 +0200

    initialized fake factor application script and restructured shell scripts

--- fake-factors/run_fake_factors.sh
+++ fake-factors/run_fake_factors.sh
@@ -2,16 +2,5 @@
 
 ERA=$1
 
-source utils/setup_cvmfs_sft.sh
-source utils/setup_python.sh
-source utils/setup_samples.sh $ERA
-
-python fake-factors/produce_shapes.py \
-        --directory $ARTUS_OUTPUTS \
-        --et-friend-directory $ARTUS_FRIENDS_ET \
-        --mt-friend-directory $ARTUS_FRIENDS_MT \
-        --tt-friend-directory $ARTUS_FRIENDS_TT \
-        --datasets $KAPPA_DATABASE \
-        --era $ERA \
-        --tag $ERA \
-        --num-threads 16 # & # NOTE: We are at the file descriptor limit.
+#./fake-factors/produce_shapes.sh $ERA
+./fake-factors/calculate_fake_factors.sh $ERA /storage/c/swozniewski/SM_Htautau/fake_factor_friends
