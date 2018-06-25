df1bb7e35b5bd66f0b16a952b4768cf83a5c4d85
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-06-25 12:48:11 +0200

    perform signal strength fit starting from expected signal strength for optimal Asimov fit

--- combine/signal_strength.sh
+++ combine/signal_strength.sh
@@ -19,14 +19,14 @@ then
         -m 125 \
         -o ${ERA}_workspace.root \
         -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel \
-        --PO '"map=^.*/ggH125_0J.?$:r_ggH_0J[0,-30,30]"' \
-        --PO '"map=^.*/ggH125_1J.?$:r_ggH_1J[0,-30,30]"' \
-        --PO '"map=^.*/ggH125_GE2J.?$:r_ggH_GE2J[0,-30,30]"' \
-        --PO '"map=^.*/ggH125_VBFTOPO.?$:r_ggH_VBFTOPO[0,-30,30]"' \
-        --PO '"map=^.*/qqH125_VBFTOPO_JET3VETO.?$:r_qqH_VBFTOPO_JET3VETO[0,-30,30]"' \
-        --PO '"map=^.*/qqH125_VBFTOPO_JET3.?$:r_qqH_VBFTOPO_JET3[0,-30,30]"' \
-        --PO '"map=^.*/qqH125_REST.?$:r_qqH_REST[0,-30,30]"' \
-        --PO '"map=^.*/qqH125_PTJET1_GT200.?$:r_qqH_PTJET1_GT200[0,-30,30]"' \
+        --PO '"map=^.*/ggH125_0J.?$:r_ggH_0J[1,-30,30]"' \
+        --PO '"map=^.*/ggH125_1J.?$:r_ggH_1J[1,-30,30]"' \
+        --PO '"map=^.*/ggH125_GE2J.?$:r_ggH_GE2J[1,-30,30]"' \
+        --PO '"map=^.*/ggH125_VBFTOPO.?$:r_ggH_VBFTOPO[1,-30,30]"' \
+        --PO '"map=^.*/qqH125_VBFTOPO_JET3VETO.?$:r_qqH_VBFTOPO_JET3VETO[1,-30,30]"' \
+        --PO '"map=^.*/qqH125_VBFTOPO_JET3.?$:r_qqH_VBFTOPO_JET3[1,-30,30]"' \
+        --PO '"map=^.*/qqH125_REST.?$:r_qqH_REST[1,-30,30]"' \
+        --PO '"map=^.*/qqH125_PTJET1_GT200.?$:r_qqH_PTJET1_GT200[1,-30,30]"' \
         -i ${ERA}_datacard.txt
 
     # Fit
