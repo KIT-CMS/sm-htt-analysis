5ff60fbf8f6eb978f55e4dcadae9c6db0561444e
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2018-06-27 17:16:25 +0200

    add shape production for fake factor method

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -7,6 +7,7 @@ ARTUS_OUTPUTS_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-04-26/me
 ARTUS_FRIENDS_ET_2016=/storage/c/wunsch/Artus_2018-04-26/et_keras_5
 ARTUS_FRIENDS_MT_2016=/storage/c/wunsch/Artus_2018-04-26/mt_keras_5
 ARTUS_FRIENDS_TT_2016=/storage/c/wunsch/Artus_2018-04-26/tt_keras_5
+ARTUS_FRIENDS_FAKE_FACTOR=/storage/c/swozniewski/SM_Htautau/fake_factor_friends
 
 # Error-handling
 if [[ $ERA == *"2016"* ]]
