4615098d513c0cfc147980893fccc58ad226cd57
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2018-07-03 01:21:33 +0200

    update shell script

--- fake-factors/run_fake_factors.sh
+++ fake-factors/run_fake_factors.sh
@@ -2,5 +2,5 @@
 
 ERA=$1
 
-#./fake-factors/produce_shapes.sh $ERA
-./fake-factors/calculate_fake_factors.sh $ERA /storage/c/swozniewski/SM_Htautau/fake_factor_friends
+./fake-factors/produce_shapes.sh $ERA
+./fake-factors/calculate_fake_factors.sh $ERA /storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-06-24/fake_factor_friends
