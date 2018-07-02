e02f5db5caa888d2d434acb7662273e3716949fb
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-07-02 15:07:32 +0200

    use correct CombineHarvester tag; adapt produce_datacard to call MorphingSM2017 binary

--- datacards/produce_datacard.sh
+++ datacards/produce_datacard.sh
@@ -8,9 +8,24 @@ STXS_SIGNALS=$2
 STXS_CATEGORIES=$3
 CHANNELS=${@:4}
 
-python datacards/produce_datacard.py \
-    --era $ERA \
-    --channels $CHANNELS \
-    --stxs-signals $STXS_SIGNALS \
-    --stxs-categories $STXS_CATEGORIES \
-    --shapes ${ERA}_shapes.root
+#USE_DATACARDPRODUCER=1
+if [ -n "$USE_DATACARDPRODUCER" ]; then
+    python datacards/produce_datacard.py \
+        --era $ERA \
+        --channels $CHANNELS \
+        --stxs-signals $STXS_SIGNALS \
+        --stxs-categories $STXS_CATEGORIES \
+        --shapes ${ERA}_shapes.root
+fi
+
+USE_COMBINEHARVESTER=1
+if [ -n "$USE_COMBINEHARVESTER" ]; then
+    CMSSW_7_4_7/bin/slc6_amd64_gcc491/MorphingSM2017 \
+        --base_path="./" \
+        --input_folder_mt="." \
+        --real_data=false \
+        --jetfakes=false \
+        --postfix="-ML" \
+        --auto_rebin=true \
+        --output_folder="output"
+fi
--- utils/init_cmssw.sh
+++ utils/init_cmssw.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Export CMSSW
 export SCRAM_ARCH=slc6_amd64_gcc491
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-scramv1 project CMSSW CMSSW_7_4_7
+scram project CMSSW CMSSW_7_4_7
 cd CMSSW_7_4_7/src
 
-git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit -b 74x-root6
-git clone https://github.com/cms-analysis/CombineHarvester.git CombineHarvester -b analysis-HIG-16-006-freeze-080416
+# Clone combine
+git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit HiggsAnalysis/CombinedLimit
+cd HiggsAnalysis/CombinedLimit
+git fetch origin
+git checkout v6.3.2
+cd ../..
 
-scramv1 b -j 24
-scramv1 b python
+# Clone CombineHarvester
+git clone https://github.com/cms-analysis/CombineHarvester CombineHarvester -b SMHTT2017-dev
+
+# Build
+scram b -j 24
+scram b python
