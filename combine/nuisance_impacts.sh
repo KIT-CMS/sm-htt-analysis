a7db624e46966c25af2a4ebddbe3fb48a1bfd6c8
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-07-06 16:39:44 +0200

    Use workspaces instead of txt datacards

--- combine/nuisance_impacts.sh
+++ combine/nuisance_impacts.sh
@@ -4,7 +4,6 @@ ERA=$1
 
 source utils/setup_cmssw.sh
 
-text2workspace.py -m 125 ${ERA}_datacard.txt -o ${ERA}_workspace.root
 combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --doInitialFit
 combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --doFits --parallel 20
 combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --output ${ERA}_impacts.json
--- combine/signal_strength.sh
+++ combine/signal_strength.sh
@@ -8,7 +8,7 @@ source utils/setup_cmssw.sh
 if [ $STXS_SIGNALS == 0 ]
 then
     # Fit
-    combine -M MaxLikelihoodFit -m 125 ${ERA}_datacard.txt --robustFit 1 -n $ERA
+    combine -M MaxLikelihoodFit -m 125 ${ERA}_workspace.root --robustFit 1 -n $ERA
 fi
 
 if [ $STXS_SIGNALS == 1 ]
@@ -17,7 +17,7 @@ then
     combineTool.py \
         -M T2W \
         -m 125 \
-        -o ${ERA}_workspace.root \
+        -o ${ERA}_workspace_STXS_stage1.root \
         -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel \
         --PO '"map=^.*/ggH125_0J.?$:r_ggH_0J[1,-30,30]"' \
         --PO '"map=^.*/ggH125_1J.?$:r_ggH_1J[1,-30,30]"' \
@@ -27,8 +27,8 @@ then
         --PO '"map=^.*/qqH125_VBFTOPO_JET3.?$:r_qqH_VBFTOPO_JET3[1,-30,30]"' \
         --PO '"map=^.*/qqH125_REST.?$:r_qqH_REST[1,-30,30]"' \
         --PO '"map=^.*/qqH125_PTJET1_GT200.?$:r_qqH_PTJET1_GT200[1,-30,30]"' \
-        -i ${ERA}_datacard.txt
+        -i ${ERA}_workspace.root
 
     # Fit
-    combineTool.py -M MultiDimFit -m 125 -d ${ERA}_workspace.root --algo singles --robustFit 1 -n $ERA
+    combineTool.py -M MultiDimFit -m 125 -d ${ERA}_workspace_STXS_stage1.root --algo singles --robustFit 1 -n $ERA
 fi
