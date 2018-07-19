9a9c782c9f2144f9db3d765a142f1b42853e13e7
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-07-19 17:17:16 +0200

    Adapt plot_shapes.sh with fake factor and embedding flags

--- plotting/plot_shapes.sh
+++ plotting/plot_shapes.sh
@@ -6,7 +6,9 @@ source utils/setup_python.sh
 ERA=$1
 STXS_SIGNALS=$2
 STXS_CATEGORIES=$3
-CHANNELS=${@:4}
+JETFAKES=$4
+EMBEDDING=$5
+CHANNELS=${@:6}
 
 if [ $STXS_SIGNALS == 1 ]
 then
@@ -14,11 +16,23 @@ then
     exit
 fi
 
+if [ $EMBEDDING == 1 ]
+then
+    echo "[ERROR] Plotting for embedding ist not yet implemented."
+    exit
+fi
+
+JETFAKES_ARG=""
+if [ $JETFAKES == 1 ]
+then
+    JETFAKES_ARG="--fake-factor"
+fi
+
 mkdir -p plots
 for FILE in "${ERA}_datacard_shapes_prefit.root" "${ERA}_datacard_shapes_postfit_sb.root"
 do
     for OPTION in "" "--png"
     do
-        ./plotting/plot_shapes.py -i $FILE -c $CHANNELS -e $ERA $OPTION --stxs-categories $STXS_CATEGORIES
+        ./plotting/plot_shapes.py -i $FILE -c $CHANNELS -e $ERA $OPTION --stxs-categories $STXS_CATEGORIES $JETFAKES_ARG
     done
 done
