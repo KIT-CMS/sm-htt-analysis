370e8f04e140405e35a11470b424ac4afea22598
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-07-24 08:50:48 +0200

    set path to new training and fake-factors

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -3,11 +3,11 @@
 ERA=$1
 
 # Samples Run2016
-ARTUS_OUTPUTS_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-07-06/merged
-ARTUS_FRIENDS_ET_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-07-06/et_keras_1
-ARTUS_FRIENDS_MT_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-07-06/mt_keras_1
-ARTUS_FRIENDS_TT_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-07-06/tt_keras_1
-ARTUS_FRIENDS_FAKE_FACTOR=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-07-06/fake_factor_friends_NNbinned2
+ARTUS_OUTPUTS_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-07-17/merged
+ARTUS_FRIENDS_ET_2016=/storage/c/wunsch/Artus_2018-07-17/et_keras_1
+ARTUS_FRIENDS_MT_2016=/storage/c/wunsch/Artus_2018-07-17/mt_keras_1
+ARTUS_FRIENDS_TT_2016=/storage/c/wunsch/Artus_2018-07-17/tt_keras_1
+ARTUS_FRIENDS_FAKE_FACTOR=/storage/c/wunsch/Artus_2018-07-17/fake_factor_friends_NN_score
 
 #ARTUS_OUTPUTS_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-04-26/merged
 #ARTUS_FRIENDS_ET_2016=/storage/c/wunsch/Artus_2018-04-26/et_keras_5
