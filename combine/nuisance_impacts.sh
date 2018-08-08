f9b1b0975ab9076750142d1804931b3be51ffc29
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-08-08 08:32:58 +0200

    fix combine calls for nuisance impacts

--- combine/nuisance_impacts.sh
+++ combine/nuisance_impacts.sh
@@ -5,6 +5,6 @@ ERA=$1
 source utils/setup_cmssw.sh
 
 combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --doInitialFit --robustFit 1 -t -1 --expectSignal=1 --parallel 20
-combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --doFits --parallel 20 --robustFit 1 -t -1 --expectSignal=1 --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP
+combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --doFits --parallel 20 --robustFit 1 -t -1 --expectSignal=1 # --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP # with this options it does not seem to find an end
 combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --output ${ERA}_impacts.json
 plotImpacts.py -i ${ERA}_impacts.json -o ${ERA}_impacts
