0efdec219618add0967e38cf6363f1f954b26e15
ArturAkh <artur.akhmetshin@cern.ch>
2018-08-22 20:54:56 +0200

    setup_samples_2017.sh: switch to original samples, buggy postsync for time-being

--- utils/setup_samples_2017.sh
+++ utils/setup_samples_2017.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # general corrections applied: MetFilters, 2016 BTag efficiencies & Fall17 V2 CSV used from btag corrections
 
-ARTUS_OUTPUTS=/storage/b/akhmet/merged_files_from_naf/21_08_2018_Run2MSSMAnalysis_postsync
+ARTUS_OUTPUTS=/storage/b/akhmet/merged_files_from_naf/21_08_2018_Run2MSSMAnalysis
 KAPPA_DATABASE=/portal/ekpbms1/home/akhmet/workdir/SkimmingMain/CMSSW_9_4_9/src/Kappa/Skimming/data/datasets.json
 
 # if you are using NAF for post-processing
-#ARTUS_OUTPUTS=/nfs/dust/cms/user/aakhmets/merged_artus_files_naf/21_08_2018_Run2MSSMAnalysis_postsync
+ARTUS_OUTPUTS=/nfs/dust/cms/user/aakhmets/merged_artus_files_naf/21_08_2018_Run2MSSMAnalysis
 #KAPPA_DATABASE=/nfs/dust/cms/user/aakhmets/AnalysisMain/CMSSW_9_4_9/src/Kappa/Skimming/data/datasets.json
