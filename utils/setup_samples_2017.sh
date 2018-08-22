485f287cf09a374838b0993d02a25d0682036da2
ArturAkh <artur.akhmetshin@cern.ch>
2018-08-22 20:58:45 +0200

    fix typo

--- utils/setup_samples_2017.sh
+++ utils/setup_samples_2017.sh
@@ -5,5 +5,5 @@ ARTUS_OUTPUTS=/storage/b/akhmet/merged_files_from_naf/21_08_2018_Run2MSSMAnalysi
 KAPPA_DATABASE=/portal/ekpbms1/home/akhmet/workdir/SkimmingMain/CMSSW_9_4_9/src/Kappa/Skimming/data/datasets.json
 
 # if you are using NAF for post-processing
-ARTUS_OUTPUTS=/nfs/dust/cms/user/aakhmets/merged_artus_files_naf/21_08_2018_Run2MSSMAnalysis
+#ARTUS_OUTPUTS=/nfs/dust/cms/user/aakhmets/merged_artus_files_naf/21_08_2018_Run2MSSMAnalysis
 #KAPPA_DATABASE=/nfs/dust/cms/user/aakhmets/AnalysisMain/CMSSW_9_4_9/src/Kappa/Skimming/data/datasets.json
