e83d3174fdc7368110f744adbdf0c1ec0ca7c86d
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-09-25 19:49:10 +0200

    add jdl_creator to PYTHONPATH

--- utils/setup_python.sh
+++ utils/setup_python.sh
@@ -6,3 +6,4 @@ export PYTHONPATH=$PYTHONPATH:$PWD/Dumbledraw
 export PYTHONPATH=$PYTHONPATH:$PWD/tensorflow_derivative
 export PYTHONPATH=$PYTHONPATH:$PWD/htt-ml
 export PYTHONPATH=$PYTHONPATH:$PWD/fake-factors
+export PYTHONPATH=$PYTHONPATH:$PWD/jdl_creator
