a53456dd55e219378f4b50ba42347b3d504ebb87
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-10-22 10:48:24 +0200

    prevent cleaning in gof workspace if not desired

--- gof/job.sh
+++ gof/job.sh
@@ -13,7 +13,7 @@ echo "Channel:" $CHANNEL
 VARIABLE=$3
 echo "Variable:" $VARIABLE
 
-OUTPUT_DIR=/ceph/wunsch/jobs_gof_${ERA}/${ERA}_${CHANNEL}_${VARIABLE}
+OUTPUT_DIR=/ceph/wunsch/jobs_gof/${ERA}_${CHANNEL}_${VARIABLE}
 echo "Output directory:" $OUTPUT_DIR
 
 BASE_PATH=/portal/ekpbms2/home/wunsch/workspace/sm-htt-analysis
@@ -27,12 +27,11 @@ echo "Where am I?" `pwd`
 
 echo "### Start working"
 
-cp -r $BASE_PATH src
-cd src
+cp -r $BASE_PATH workspace
+cd workspace
 
 sed -i "s%CMSSW_7_4_7%${BASE_PATH}/CMSSW_7_4_7%g" utils/setup_cmssw.sh
 
-./utils/clean.sh
 ./gof/run_gof.sh $ERA $CHANNEL $VARIABLE
 
 mkdir -p $OUTPUT_DIR
