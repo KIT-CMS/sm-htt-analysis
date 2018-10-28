526faf22db459754e2ffb08589e5f68efa4737cd
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-10-28 20:27:24 +0100

    [2016] push new training

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -4,9 +4,9 @@ ERA=$1
 
 # Samples Run2016
 ARTUS_OUTPUTS_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-08-21/merged
-ARTUS_FRIENDS_ET_2016=/ceph/wunsch/Artus_2018-08-21/et_keras_2
-ARTUS_FRIENDS_MT_2016=/ceph/wunsch/Artus_2018-08-21/mt_keras_2
-ARTUS_FRIENDS_TT_2016=/ceph/wunsch/Artus_2018-08-21/tt_keras_2
+ARTUS_FRIENDS_ET_2016=/ceph/wunsch/Artus_2018-08-21/et_keras_3
+ARTUS_FRIENDS_MT_2016=/ceph/wunsch/Artus_2018-08-21/mt_keras_3
+ARTUS_FRIENDS_TT_2016=/ceph/wunsch/Artus_2018-08-21/tt_keras_3
 ARTUS_FRIENDS_FAKE_FACTOR_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-08-21/fake_factor_friends_njets_mvis
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2016=/storage/c/swozniewski/SM_Htautau/ntuples/Artus_2018-08-21/fake_factor_friends_njets_mvis_incl
 
