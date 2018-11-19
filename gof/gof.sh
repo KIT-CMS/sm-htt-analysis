af2f534b65548c9293d003b7b7145562dd472871
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-11-19 23:05:32 +0100

    split gof production and push number of toys to 500

--- gof/gof.sh
+++ gof/gof.sh
@@ -4,18 +4,37 @@ source utils/setup_cmssw.sh
 
 ERA=$1
 DATACARD=${ERA}_workspace.root
-SEED=1234
 MASS=125
-NUM_TOYS=300
+NUM_TOYS=50 # multiply x10
 
 # Get test statistic value
 combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD
 
 # Throw toys
-combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s $SEED -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1230 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1231 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1232 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1233 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1234 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1235 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1236 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1237 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1238 -t $NUM_TOYS
+combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s 1239 -t $NUM_TOYS
 
 # Collect results
-combineTool.py -M CollectGoodnessOfFit --input higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.$SEED.root --output gof.json
+combineTool.py -M CollectGoodnessOfFit --input \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1230.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1231.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1232.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1233.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1234.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1235.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1236.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1237.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1238.root \
+    higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.1239.root \
+    --output gof.json
 
 # Plot
 plotGof.py --statistic saturated --mass $MASS.0 --output gof gof.json
