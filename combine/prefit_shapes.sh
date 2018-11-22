91a1111f6dc88bb94e3d05c21700f756841ea331
janekbechtel <janek.bechtel@student.kit.edu>
2018-11-22 09:59:41 +0100

    Add labels to Dumbledraw and fix x-ticks of prefit plots

--- combine/prefit_shapes.sh
+++ combine/prefit_shapes.sh
@@ -5,4 +5,4 @@ ERA=$1
 source utils/setup_cmssw.sh
 
 # Prefit shapes
-PostFitShapesFromWorkspace -m 125 -w ${ERA}_workspace.root -o ${ERA}_datacard_shapes_prefit.root
+PostFitShapesFromWorkspace -m 125 -w ${ERA}_workspace.root -o ${ERA}_datacard_shapes_prefit.root -d output/${ERA}_smhtt/cmb/125/combined.txt.cmb
