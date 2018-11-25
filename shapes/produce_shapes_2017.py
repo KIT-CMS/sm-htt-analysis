9824da5eebe1571b55adccd2eac016356f628ef2
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-11-25 20:10:02 +0100

    [2017] remove VH and HTT signals since we do not have these processes

--- shapes/produce_shapes_2017.py
+++ shapes/produce_shapes_2017.py
@@ -417,7 +417,7 @@ def main(args):
     # Nominal histograms
     if args.gof_channel == None:
         signal_nicks = [
-            "HTT", "VH", "ggH", "qqH", "qqH_VBFTOPO_JET3VETO", "qqH_VBFTOPO_JET3",
+            "ggH", "qqH", "qqH_VBFTOPO_JET3VETO", "qqH_VBFTOPO_JET3",
             "qqH_REST", "qqH_PTJET1_GT200", "qqH_VH2JET", "ggH_0J",
             "ggH_1J_PTH_0_60", "ggH_1J_PTH_60_120", "ggH_1J_PTH_120_200",
             "ggH_1J_PTH_GT200", "ggH_GE2J_PTH_0_60", "ggH_GE2J_PTH_60_120",
