b2e865f9d3251ca649d95b3d930411ff5a16de9a
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-11-26 19:20:00 +0100

    [gof] fix the gof toolchain with the changes from the combination

--- gof/produce_datacard.sh
+++ gof/produce_datacard.sh
@@ -14,41 +14,23 @@ GOF_CATEGORY_NAME=${CHANNEL}_${VARIABLE}
 source utils/setup_cmssw.sh
 source utils/setup_python.sh
 
-#USE_DATACARDPRODUCER=1
-if [ -n "$USE_DATACARDPRODUCER" ]; then
-python datacards/produce_datacard.py \
-    --era $ERA \
-    --channels $CHANNEL \
-    --gof $VARIABLE \
-    --shapes ${ERA}_shapes.root \
-    --use-data-for-observation
-fi
+# Remove output directory
+rm -rf output/${ERA}_smhtt
 
-USE_COMBINEHARVESTER=1
-if [ -n "$USE_COMBINEHARVESTER" ]; then
-    # Remove output directory
-    rm -rf output/ ${ERA}_workspace.root
-
-    # Create datacards
-    $CMSSW_BASE/bin/slc6_amd64_gcc491/MorphingSM2017 \
-        --base_path=$PWD \
-        --input_folder_mt="/" \
-        --input_folder_et="/" \
-        --input_folder_tt="/" \
-        --real_data=true \
-        --jetfakes=$JETFAKES \
-        --embedding=$EMBEDDING \
-        --postfix="-ML" \
-        --channel=$CHANNEL \
-        --auto_rebin=true \
-        --stxs_signals=$STXS_SIGNALS \
-        --categories="gof" \
-        --gof_category_name=$GOF_CATEGORY_NAME \
-        --era=$ERA \
-        --output="${ERA}_smhtt"
-
-    # Merge datacards to workspace and define signals to be fitted
-    DATACARD_PATH=${PWD}/output/${ERA}_smhtt/cmb/125
-    combineTool.py -M T2W -o workspace.root -i ${DATACARD_PATH} --parallel $NUM_THREADS
-    cp $DATACARD_PATH/workspace.root ${ERA}_workspace.root
-fi
+# Create datacards
+$CMSSW_BASE/bin/slc6_amd64_gcc491/MorphingSM2017 \
+    --base_path=$PWD \
+    --input_folder_mt="/" \
+    --input_folder_et="/" \
+    --input_folder_tt="/" \
+    --real_data=true \
+    --jetfakes=$JETFAKES \
+    --embedding=$EMBEDDING \
+    --postfix="-ML" \
+    --channel=$CHANNEL \
+    --auto_rebin=true \
+    --stxs_signals=$STXS_SIGNALS \
+    --categories="gof" \
+    --gof_category_name=$GOF_CATEGORY_NAME \
+    --era=$ERA \
+    --output="${ERA}_smhtt"
--- gof/run_gof.sh
+++ gof/run_gof.sh
@@ -32,6 +32,9 @@ JETFAKES=1
 EMBEDDING=1
 ./gof/produce_datacard.sh $ERA $CHANNEL $VARIABLE $JETFAKES $EMBEDDING
 
+# Build workspace
+./datacards/produce_workspace.sh $ERA "inclusive" | tee ${ERA}_produce_workspace_inclusive.log
+
 # Run goodness of fit test
 ./gof/gof.sh $ERA
 
