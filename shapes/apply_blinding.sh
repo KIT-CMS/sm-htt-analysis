3fedbf4bd8aaf24c50d98e5b14b9f3ea243ced5e
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-11-26 19:43:30 +0100

    [shapes] fix blinding script for new jetfakes estimation

--- shapes/apply_blinding.sh
+++ shapes/apply_blinding.sh
@@ -7,4 +7,4 @@ source utils/setup_cvmfs_sft.sh
 python shapes/apply_blinding.py ${ERA}_shapes.root \
     --threshold 0.5 --uncertainty 0.09 \
     --signal-processes ggH qqH VH \
-    --exclude-categories _ss _B _sumW _sumEWKZ
+    --exclude-categories _ss _B _sumW _sumEWKZ _FF
