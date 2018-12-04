6f7c0a996362cf18216ab202c332cb2e0112babc
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-12-04 18:18:30 +0100

    [ml] register new tt training (2017) with pt_tt channel cut

--- run_ml.sh
+++ run_ml.sh
@@ -4,6 +4,7 @@ ERA=$1
 CHANNEL=$2
 
 ./ml/create_training_dataset.sh $ERA $CHANNEL
+#./ml/sum_training_weights.sh $ERA
 ./ml/run_training.sh $ERA $CHANNEL
 ./ml/run_testing.sh $ERA $CHANNEL
 ./ml/run_application.sh $ERA $CHANNEL
