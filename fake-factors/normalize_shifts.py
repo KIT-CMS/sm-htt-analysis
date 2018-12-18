33f02c1d20fa100c0b0f542c929c12b430efae7f
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2018-12-18 17:23:59 +0100

    add fake factor shape interpolation

--- fake-factors/normalize_shifts.py
+++ fake-factors/normalize_shifts.py
@@ -27,6 +27,19 @@ def setup_logging(output_file, level=logging.DEBUG):
     logger.addHandler(file_handler)
 
 
+def interpolateHistos(self, h1_old, h2_old):
+
+        h1,h2 = copy.deepcopy(h1_old),copy.deepcopy(h2_old)
+
+        nbins=h1_old.GetNbinsX()
+        for ibin in xrange(nbins+2):     #includes under- and overflow
+            cont1,cont2=h1_old.GetBinContent(ibin),h2_old.GetBinContent(ibin)
+            h1.SetBinContent( ibin, cont2* float(nbins-ibin)/nbins + cont1* float(ibin)/nbins )
+            h2.SetBinContent( ibin, cont1* float(nbins-ibin)/nbins + cont2* float(ibin)/nbins )
+
+        return h1,h2
+
+
 def main(args):
     file_ = ROOT.TFile(args.input, "UPDATE")
     for key in file_.GetListOfKeys():
@@ -47,6 +60,7 @@ def main(args):
         shift = split[7]
         if shift[-4:] == "Down":
             shift_type = "down"
+            continue #run down together with up
         elif shift[-2:] == "Up":
             shift_type = "up"
         else:
@@ -56,11 +70,14 @@ def main(args):
 
         # Renormalize if systematic has sub-string _ff_
         if "_ff_" in name:
-            h_shift = file_.Get(name)
-            if h_shift == None:
+            h_shift_raw = file_.Get(name)
+            h_shift_down_raw = file_.Get(name.replace("Up", "Down"))
+            if h_shift_raw == None or h_shift_down_raw == None:
                 logger.critical("Failed to get shape syst. histogram %s.",
                                 name)
                 raise Exception
+            
+            h_shift, h_shift_down = interpolateHistos(h_shift_raw, h_shift_down_raw)
 
             nominal = "#" + "#".join(split[:-1]) + "#"
             h_nominal = file_.Get(nominal)
@@ -69,17 +86,23 @@ def main(args):
                 raise Exception
 
             norm_shift = h_shift.Integral()
+            norm_shift_down = h_shift_down.Integral()
             norm_nominal = h_nominal.Integral()
-            if norm_shift == 0:
+            if norm_shift == 0 or norm_shift_down == 0:
                 logger.warning("Found shift with integral of zero for systematic %s. Continue.",
                         name)
                 continue
             scale = norm_nominal / norm_shift
+            scale_down = norm_nominal / norm_shift_down
             logger.debug(
                 "Renormalize systematic %s (%f) with integral of %s (%f): %f",
                 name, norm_shift, nominal, norm_nominal, scale)
             h_shift.Scale(scale)
-            h_shift.Write()
+            logger.debug(
+                "Renormalize systematic %s (%f) with integral of %s (%f): %f",
+                name.replace("Up", "Down"), norm_shift, nominal, norm_nominal, scale)
+            h_shift_down.Scale(scale_down)
+            h_shift_down.Write()
     logger.info("Successfully normalized fake factor systematics to nominal.")
     file_.Close()
 
