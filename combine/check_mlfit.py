8edb69bd6c20b83cd0118017edb60ec88d09c3c7
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-01-07 10:23:32 +0100

    [combine] Improve script which checks ML fit validity by covariance quality

--- combine/check_mlfit.py
+++ combine/check_mlfit.py
@@ -6,6 +6,16 @@ import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True  # disable ROOT internal argument parser
 import sys
 
+# Documentation of error codes for covariance matrices
+# https://github.com/root-project/root/blob/master/roofit/roofitcore/src/RooFitResult.cxx#L506-L512
+"""
+case -1 : os << "Unknown, matrix was externally provided" ; break ;
+case 0  : os << "Not calculated at all" ; break ;
+case 1  : os << "Approximation only, not accurate" ; break ;
+case 2  : os << "Full matrix, but forced positive-definite" ; break ;
+case 3  : os << "Full, accurate covariance matrix" ; break ;
+"""
+
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -17,7 +27,8 @@ if __name__ == "__main__":
         if fit == None:
             raise Exception("[ERROR] {} not found in file {}.".format(name, filename))
         status = fit.status()
-        if status == 0:
-            print("[INFO] Fit {} from file {} is ok.".format(name, filename))
+        cov_qual = fit.covQual()
+        if status == 0 and cov_qual == 3:
+            print("[INFO] Fit {} from file {} is ok (status: {}, covQual: {}).".format(name, filename, status, cov_qual))
         else:
-            raise Exception("[ERROR] Fit {} from file {} is invalid.".format(name, filename))
+            raise Exception("[ERROR] Fit {} from file {} is invalid (status: {}, covQual: {}).".format(name, filename, status, cov_qual))
