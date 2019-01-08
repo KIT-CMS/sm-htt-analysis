22741cc3d3929164ecdf5240873b0eef6e4694b9
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-01-08 17:54:05 +0100

    [combine] reenable robustHesse for poi corr plot

--- combine/poi_correlation.sh
+++ combine/poi_correlation.sh
@@ -9,6 +9,6 @@ source utils/setup_cmssw.sh
 # combining all eras
 ulimit -s unlimited
 
-#./combine/signal_strength.sh ${ERA} "robustHesse"
+./combine/signal_strength.sh ${ERA} "robustHesse"
 
 python combine/plot_poi_correlation.py ${ERA}_${STXS_FIT} fitDiagnostics${ERA}.root
