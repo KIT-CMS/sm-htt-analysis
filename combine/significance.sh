3446f0d9c339016cf1c90de02189ddcd17ac740f
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-01-23 14:09:40 +0100

    [combine] include autoMCStats options for significance

--- combine/significance.sh
+++ combine/significance.sh
@@ -4,4 +4,12 @@ ERA=$1
 
 source utils/setup_cmssw.sh
 
-combine -M ProfileLikelihood -t -1 --expectSignal 1 --toysFrequentist --significance -m 125 -n $ERA ${ERA}_workspace.root
+# NOTE: toysFrequentist computes postfit signifiance
+combine -M ProfileLikelihood \
+    -t -1 --expectSignal 1 \
+    --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 \
+    --significance \
+    -m 125 -n $ERA \
+    --toysFrequentist \
+    -v 1 \
+    ${ERA}_workspace.root
