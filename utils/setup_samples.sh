7dd226ac71c87c2b19cffb60476a593c32160f5b
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2019-01-23 14:40:22 +0100

    update shape-producer and according samples

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -3,19 +3,19 @@
 ERA=$1
 
 # Samples Run2016
-ARTUS_OUTPUTS_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016/merged
-ARTUS_FRIENDS_ET_2016=/ceph/wunsch/Artus16_Nov/et_keras_20181127
-ARTUS_FRIENDS_MT_2016=/ceph/wunsch/Artus16_Nov/mt_keras_20181127
-ARTUS_FRIENDS_TT_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_NovPU/tt_keras_20181215
-ARTUS_FRIENDS_FAKE_FACTOR_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016/fake_factor_friends_njets_mvis_NEW_NN_Dec16
+ARTUS_OUTPUTS_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/merged
+ARTUS_FRIENDS_ET_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/et_keras_20181127
+ARTUS_FRIENDS_MT_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/mt_keras_20181127
+ARTUS_FRIENDS_TT_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/tt_keras_20181216
+ARTUS_FRIENDS_FAKE_FACTOR_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/fake_factor_friends_njets_mvis_NEW_NN_Jan20
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016/fake_factor_friends_njets_mvis_NEW_incl_Dec15
 
 # Samples Run2017
-ARTUS_OUTPUTS_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Dec/merged
-ARTUS_FRIENDS_ET_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Dec/et_keras_20181202
-ARTUS_FRIENDS_MT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Dec/mt_keras_20181202
-ARTUS_FRIENDS_TT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Dec/tt_keras_20181205
-ARTUS_FRIENDS_FAKE_FACTOR_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Dec/fake_factor_friends_njets_mvis_NEW_NN_Dec11
+ARTUS_OUTPUTS_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/merged
+ARTUS_FRIENDS_ET_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/et_keras_20181202
+ARTUS_FRIENDS_MT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/mt_keras_20181202
+ARTUS_FRIENDS_TT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/tt_keras_20181205
+ARTUS_FRIENDS_FAKE_FACTOR_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/fake_factor_friends_njets_mvis_NEW_NN_Jan20
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_NovPU/fake_factor_friends_njets_mvis_NEW_incl_Dec11
 
 # Error-handling
@@ -41,4 +41,4 @@ else
 fi
 
 # Kappa database
-KAPPA_DATABASE=/ceph/akhmet/kappa_database/datasets_2018_11_20.json
+KAPPA_DATABASE=/ceph/swozniewski/kappa_database/datasets_2019_01_20.json
