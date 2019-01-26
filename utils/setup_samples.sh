a91c458c82dc08d29a8cf65d04c0a94b904bcff8
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-01-26 16:11:02 +0100

    [ml] add new 2017 training

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -11,10 +11,10 @@ ARTUS_FRIENDS_FAKE_FACTOR_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016/fake_factor_friends_njets_mvis_NEW_incl_Dec15
 
 # Samples Run2017
-ARTUS_OUTPUTS_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/merged
-ARTUS_FRIENDS_ET_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/et_keras_20181202
-ARTUS_FRIENDS_MT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/mt_keras_20181202
-ARTUS_FRIENDS_TT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/tt_keras_20181205
+ARTUS_OUTPUTS_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/merged_fixedjets
+ARTUS_FRIENDS_ET_2017=/ceph/wunsch/Artus17_Jan/et_keras_20190125
+ARTUS_FRIENDS_MT_2017=/ceph/wunsch/Artus17_Jan/mt_keras_20190125
+ARTUS_FRIENDS_TT_2017=/ceph/wunsch/Artus17_Jan/tt_keras_20190125
 ARTUS_FRIENDS_FAKE_FACTOR_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/fake_factor_friends_njets_mvis_NEW_NN_Jan20
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_NovPU/fake_factor_friends_njets_mvis_NEW_incl_Dec11
 
