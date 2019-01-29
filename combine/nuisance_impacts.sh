e239ce859b2e6e24f2c3ac3daad34e6f17f45cd0
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-01-29 22:27:35 +0100

    [combine] update nuisance impact commands

--- combine/nuisance_impacts.sh
+++ combine/nuisance_impacts.sh
@@ -4,7 +4,15 @@ ERA=$1
 
 source utils/setup_cmssw.sh
 
-combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --doInitialFit --robustFit 1 -t -1 --expectSignal=1 --parallel 20 --minimizerAlgoForMinos=Minuit2,Migrad
-combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --doFits --parallel 20 --robustFit 1 -t -1 --expectSignal=1 --minimizerAlgoForMinos=Minuit2,Migrad # --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP # with this options it does not seem to find an end
+combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root \
+    --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 \
+    --doInitialFit --robustFit 1 \
+    -t -1 --expectSignal=1 \
+    --parallel 32
+combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root \
+    --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 \
+    --doFits --parallel 20 --robustFit 1 \
+    -t -1 --expectSignal=1 \
+    --parallel 32
 combineTool.py -M Impacts -m 125 -d ${ERA}_workspace.root --output ${ERA}_impacts.json
 plotImpacts.py -i ${ERA}_impacts.json -o ${ERA}_impacts
