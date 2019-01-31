d3291bf9128507dc997817e4316e197d7ca6fa74
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-01-31 10:00:16 +0100

    [combine] update gof script

--- combine/gof.sh
+++ combine/gof.sh
@@ -2,18 +2,75 @@
 
 source utils/setup_cmssw.sh
 
-DATACARD=datacard.txt
-SEED=1234
+ERA=$1
 MASS=125
+TOYS=30
+
+STATISTIC=saturated
 
 # Get test statistic value
-combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0
 
 # Throw toys
-combine -M GoodnessOfFit --algo=saturated -m $MASS -d $DATACARD -s $SEED -t 100
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1230 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1231 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1232 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1233 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1234 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1235 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1236 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1237 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1238 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+combine -M GoodnessOfFit --algo=${STATISTIC} -m $MASS -d ${ERA}_workspace.root \
+        -n ${ERA} \
+        -s 1239 -t $TOYS \
+        --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0 &
+
+wait
 
 # Collect results
-combineTool.py -M CollectGoodnessOfFit --input higgsCombineTest.GoodnessOfFit.mH$MASS.root higgsCombineTest.GoodnessOfFit.mH$MASS.$SEED.root --output gof.json
+combineTool.py -M CollectGoodnessOfFit \
+    --input higgsCombine${ERA}.GoodnessOfFit.mH$MASS.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1230.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1231.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1232.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1233.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1234.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1235.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1236.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1237.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1238.root \
+        higgsCombine${ERA}.GoodnessOfFit.mH$MASS.1239.root \
+    --output gof.json
 
 # Plot
-plotGof.py --statistic saturated --mass $MASS.0 --output gof gof.json
+plotGof.py --statistic ${STATISTIC} --mass $MASS.0 --output gof gof.json
