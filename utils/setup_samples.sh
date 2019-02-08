5b1a16ce99fa5d0dc2b1d54bd96d73afb38fd0d1
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2019-02-08 16:31:58 +0100

    2017 NN friends containing ttH

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -12,9 +12,9 @@ ARTUS_FRIENDS_FAKE_FACTOR_INCL_2016=/ceph/swozniewski/SM_Htautau/ntuples/Artus16
 
 # Samples Run2017
 ARTUS_OUTPUTS_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/merged_fixedjets
-ARTUS_FRIENDS_ET_2017=/ceph/wunsch/Artus17_Jan/et_keras_20190125
-ARTUS_FRIENDS_MT_2017=/ceph/wunsch/Artus17_Jan/mt_keras_20190125
-ARTUS_FRIENDS_TT_2017=/ceph/wunsch/Artus17_Jan/tt_keras_20190125
+ARTUS_FRIENDS_ET_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/et_keras_20190125
+ARTUS_FRIENDS_MT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/mt_keras_20190125
+ARTUS_FRIENDS_TT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/tt_keras_20190125
 ARTUS_FRIENDS_FAKE_FACTOR_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/fake_factor_friends_njets_mvis_NEW_NN_Jan26
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_NovPU/fake_factor_friends_njets_mvis_NEW_incl_Dec11
 
