f96133cbd301add708b34719787c6c2e4b167d99
janekbechtel <janek.bechtel@gmail.com>
2019-02-08 16:12:55 +0100

    Add binomial bin by bins for em embedded

--- datacards/produce_datacard.sh
+++ datacards/produce_datacard.sh
@@ -24,6 +24,7 @@ $CMSSW_BASE/bin/slc6_amd64_gcc530/MorphingSM2017 \
     --input_folder_em="/" \
     --real_data=false \
     --classic_bbb=false \
+    --binomial_bbb=true \
     --jetfakes=$JETFAKES \
     --embedding=$EMBEDDING \
     --postfix="-ML" \
