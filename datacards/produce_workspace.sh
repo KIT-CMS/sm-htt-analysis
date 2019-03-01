f697b5f36a9ac65a26277042a92aa1ff1759f217
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2019-03-01 14:39:50 +0100

    update stage1 workspace production

--- datacards/produce_workspace.sh
+++ datacards/produce_workspace.sh
@@ -35,20 +35,13 @@ fi
 if [ $STXS_FIT == "stxs_stage1" ]; then
     combineTool.py -M T2W -o ${OUTPUT} -i ${INPUT} --parallel $NUM_THREADS \
         -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel \
-        --PO '"map=^.*/ggH_0J.?$:r_ggH_0J[1,-40,40]"' \
-        --PO '"map=^.*/ggH_1J_PTH_0_60.?$:r_ggH_1J_PTH_0_60[1,-40,40]"' \
-        --PO '"map=^.*/ggH_1J_PTH_60_120.?$:r_ggH_1J_PTH_60_120[1,-40,40]"' \
-        --PO '"map=^.*/ggH_1J_PTH_120_200.?$:r_ggH_1J_PTH_120_200[1,-40,40]"' \
-        --PO '"map=^.*/ggH_1J_PTH_GT200.?$:r_ggH_1J_PTH_GT200[1,-40,40]"' \
-        --PO '"map=^.*/ggH_GE2J_PTH_0_60.?$:r_ggH_GE2J_PTH_0_60[1,-60,40]"' \
-        --PO '"map=^.*/ggH_GE2J_PTH_60_120.?$:r_ggH_GE2J_PTH_60_120[1,-40,40]"' \
-        --PO '"map=^.*/ggH_GE2J_PTH_120_200.?$:r_ggH_GE2J_PTH_120_200[1,-40,40]"' \
-        --PO '"map=^.*/ggH_GE2J_PTH_GT200.?$:r_ggH_GE2J_PTH_GT200[1,-40,40]"' \
-        --PO '"map=^.*/ggH_VBFTOPO_JET3.?$:r_ggH_VBFTOPO_JET3[1,-50,40]"' \
-        --PO '"map=^.*/ggH_VBFTOPO_JET3VETO.?$:r_ggH_VBFTOPO_JET3VETO[1,-40,80]"' \
-        --PO '"map=^.*/qqH_VBFTOPO_JET3VETO.?$:r_qqH_VBFTOPO_JET3VETO[1,-40,40]"' \
-        --PO '"map=^.*/qqH_VBFTOPO_JET3.?$:r_qqH_VBFTOPO_JET3[1,-40,40]"' \
-        --PO '"map=^.*/qqH_REST.?$:r_qqH_REST[1,-40,40]"' \
-        --PO '"map=^.*/qqH_VH2JET.?$:r_qqH_VH2JET[1,-40,40]"' \
-        --PO '"map=^.*/qqH_PTJET1_GT200.?$:r_qqH_PTJET1_GT200[1,-40,40]"'
+        --PO '"map=^.*/ggH_0J.?$:r_ggH_0J[1,-70,70]"' \
+        --PO '"map=^.*/ggH_1J_PTH_0_60.?$:r_ggH_1J_PTH_0_60[1,-70,70]"' \
+        --PO '"map=^.*/ggH_1J_PTH_60_120.?$:r_ggH_1J_PTH_60_120[1,-70,70]"' \
+        --PO '"map=^.*/ggH_1J_PTH_(120_200|GT200).?$:r_ggH_1J_PTH_GT120[1,-70,70]"' \
+        --PO '"map=^.*/ggH_GE2J_PTH_(0_60|60_120|120_200|GT200).?$:r_ggH_GE2J[1,-70,70]"' \
+        --PO '"map=^.*/(ggH|qqH)_VBFTOPO_(JET3VETO|JET3).?$:r_VBFTOPO[1,-70,70]"' \
+        --PO '"map=^.*/qqH_REST.?$:r_qqH_REST[1,-70,70]"' \
+        --PO '"map=^.*/qqH_VH2JET.?$:r_qqH_VH2JET[1,-70,70]"' \
+        --PO '"map=^.*/qqH_PTJET1_GT200.?$:r_qqH_PTJET1_GT200[1,-70,70]"'
 fi
