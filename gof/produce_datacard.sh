a9094ce5f0db1df76803121d87f6c3aad2c8728c
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-04-03 12:10:21 +0200

    [gof] fix for 2016 with current CH

--- gof/produce_datacard.sh
+++ gof/produce_datacard.sh
@@ -18,19 +18,33 @@ source utils/setup_python.sh
 rm -rf output/${ERA}_smhtt
 
 # Create datacards
-$CMSSW_BASE/bin/slc6_amd64_gcc491/MorphingSM2017 \
+$CMSSW_BASE/bin/slc6_amd64_gcc530/MorphingSM2017 \
     --base_path=$PWD \
     --input_folder_mt="/" \
     --input_folder_et="/" \
     --input_folder_tt="/" \
+    --input_folder_em="/" \
     --real_data=true \
+    --classic_bbb=false \
+    --binomial_bbb=true \
     --jetfakes=$JETFAKES \
     --embedding=$EMBEDDING \
     --postfix="-ML" \
     --channel=$CHANNEL \
-    --auto_rebin=true \
+    --auto_rebin=false \
+    --rebin_categories=false \
     --stxs_signals=$STXS_SIGNALS \
     --categories="gof" \
     --gof_category_name=$GOF_CATEGORY_NAME \
     --era=$ERA \
     --output="${ERA}_smhtt"
+
+# Use Barlow-Beeston-lite approach for bin-by-bin systematics
+THIS_PWD=${PWD}
+echo $THIS_PWD
+cd output/${ERA}_smhtt/cmb/125/
+for FILE in *.txt
+do
+    sed -i '$s/$/\n * autoMCStats 0.0/' $FILE
+done
+cd $THIS_PWD
