ad4765628770b1ad9dd2c304059a39222c2800e6
Artur Gottmann <artur.gottmann@kit.edu>
2019-05-15 22:46:13 +0200

    Update samples for 2017 & produce control shapes

--- shapes/produce_control_shapes.sh
+++ shapes/produce_control_shapes.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 renice -n 19 -u `whoami`
 
+
+ERA=$1
+
 BINNING=shapes/binning.yaml
 
 source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
-source utils/setup_samples_2017.sh
+source utils/setup_samples.sh $ERA
 
 # Produce shapes
+echo outputs: $ARTUS_OUTPUTS
+
 python shapes/produce_control_shapes.py \
     --directory $ARTUS_OUTPUTS \
     --datasets $KAPPA_DATABASE \
     --binning $BINNING \
     --channels mt  \
     --fake-factor-friend-directory /storage/b/akhmet/SM_Htautau/ntuples/Artus_2018-09-01/fake_factor_friends/ \
+    --mt-friend-directory $ARTUS_FRIENDS_MT \
     --num-threads 15
 
 # Produce shapes
@@ -23,6 +29,7 @@ python shapes/produce_control_shapes.py \
     --binning $BINNING \
     --channels et \
     --fake-factor-friend-directory /storage/b/akhmet/SM_Htautau/ntuples/Artus_2018-09-01/fake_factor_friends/ \
+    --et-friend-directory $ARTUS_FRIENDS_ET \
     --num-threads 15
 
 # Produce shapes
@@ -32,6 +39,7 @@ python shapes/produce_control_shapes.py \
     --binning $BINNING \
     --channels tt \
     --fake-factor-friend-directory /storage/b/akhmet/SM_Htautau/ntuples/Artus_2018-09-01/fake_factor_friends/ \
+    --tt-friend-directory $ARTUS_FRIENDS_TT \
     --num-threads 15
     
 # Produce shapes
@@ -40,4 +48,5 @@ python shapes/produce_control_shapes.py \
     --datasets $KAPPA_DATABASE \
     --binning $BINNING \
     --channels em \
+    --em-friend-directory $ARTUS_FRIENDS_EM \
     --num-threads 15
--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -13,13 +13,13 @@ ARTUS_OUTPUTS_EM_2016=/ceph/jbechtel/2016/em_all_vars_JetFix_def
 
 
 # Samples Run2017
-ARTUS_OUTPUTS_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/merged_fixedjets
-ARTUS_FRIENDS_ET_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/et_keras_20190125
-ARTUS_FRIENDS_MT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/mt_keras_20190125
-ARTUS_FRIENDS_TT_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/tt_keras_20190125
+ARTUS_OUTPUTS_2017=/storage/b/akhmet/merged_files_from_naf/Full_2017_test_all-channels
+ARTUS_FRIENDS_ET_2017=/portal/ekpbms1/home/akhmet/workdir/FriendTreeProductionMain/CMSSW_10_2_14/src/MELA_workdir/MELA_collected
+ARTUS_FRIENDS_MT_2017=/portal/ekpbms1/home/akhmet/workdir/FriendTreeProductionMain/CMSSW_10_2_14/src/MELA_workdir/MELA_collected
+ARTUS_FRIENDS_TT_2017=/portal/ekpbms1/home/akhmet/workdir/FriendTreeProductionMain/CMSSW_10_2_14/src/MELA_workdir/MELA_collected
+ARTUS_FRIENDS_EM_2017=/portal/ekpbms1/home/akhmet/workdir/FriendTreeProductionMain/CMSSW_10_2_14/src/MELA_workdir/MELA_collected
 ARTUS_FRIENDS_FAKE_FACTOR_2017=/ceph/swozniewski/SM_Htautau/ntuples/Artus17_Jan/fake_factor_friends_njets_mvis_NEW_NN_Jan26
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2017=$ARTUS_FRIENDS_FAKE_FACTOR_2017
-ARTUS_OUTPUTS_EM_2017=/portal/ekpbms1/home/jbechtel/postprocessing/em_all_vars_new/files
 
 # Samples Run2018
 ARTUS_OUTPUTS_2018=/portal/ekpbms1/home/jbechtel/postprocessing/2018/sm-htt-analysis/files
@@ -43,22 +43,22 @@ then
 elif [[ $ERA == *"2017"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2017
-    ARTUS_OUTPUTS_EM=$ARTUS_OUTPUTS_EM_2017
     ARTUS_FRIENDS_ET=$ARTUS_FRIENDS_ET_2017
     ARTUS_FRIENDS_MT=$ARTUS_FRIENDS_MT_2017
     ARTUS_FRIENDS_TT=$ARTUS_FRIENDS_TT_2017
+    ARTUS_FRIENDS_EM=$ARTUS_FRIENDS_EM_2017
     ARTUS_FRIENDS_FAKE_FACTOR=$ARTUS_FRIENDS_FAKE_FACTOR_2017
     ARTUS_FRIENDS_FAKE_FACTOR_INCL=$ARTUS_FRIENDS_FAKE_FACTOR_INCL_2017
 elif [[ $ERA == *"2018"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2018
-    ARTUS_OUTPUTS_EM=$ARTUS_OUTPUTS_EM_2018
     ARTUS_FRIENDS_ET=$ARTUS_FRIENDS_ET_2018
     ARTUS_FRIENDS_MT=$ARTUS_FRIENDS_MT_2018
     ARTUS_FRIENDS_TT=$ARTUS_FRIENDS_TT_2018
+    ARTUS_FRIENDS_EM=$ARTUS_FRIENDS_EM_2018
     ARTUS_FRIENDS_FAKE_FACTOR=$ARTUS_FRIENDS_FAKE_FACTOR_2018
     ARTUS_FRIENDS_FAKE_FACTOR_INCL=$ARTUS_FRIENDS_FAKE_FACTOR_INCL_2018
 fi
 
 # Kappa database
-KAPPA_DATABASE=/portal/ekpbms1/home/jbechtel/postprocessing/2018/sm-htt-analysis/datasets.json
+KAPPA_DATABASE=/portal/ekpbms1/home/akhmet/workdir/AnalysisMain/CMSSW_10_2_14/src/Kappa/Skimming/data/datasets.json
