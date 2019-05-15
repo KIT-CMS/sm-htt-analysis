28adde2c46dc0dbcf599c8c938f263f5e1adb6b7
Moritz Scham <moritz.scham@student.kit.edu>
2019-05-15 17:23:52 +0200

    gpu is coosen by most available memory

--- utils/setup_cuda.sh
+++ utils/setup_cuda.sh
@@ -2,3 +2,6 @@
 
 export PATH=/usr/local/cuda-8.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+
+#Selects GPU with the least used Memory
+export CUDA_VISIBLE_DEVICES=$( nvidia-smi | grep 250W | egrep -o '[0-9]+MiB /' | egrep -o '[0-9]+' | awk '{print NR-1 " " $0}' | sort -g -k 2n | head -c 2)
