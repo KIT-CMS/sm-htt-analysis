62fd9901035b2db71635141cc2db9afe67d7ef6d
Artur Gottmann <artur.gottmann@kit.edu>
2019-05-23 19:02:17 +0200

    gof: update until synced shapes; ml: add sum weights for em

--- ml/sum_training_weights.sh
+++ ml/sum_training_weights.sh
@@ -4,6 +4,21 @@ ERA=$1
 
 source utils/setup_cvmfs_sft.sh
 
+PROCESSES="ggh qqh ztt tt ss misc db"
+CHANNEL=em
+for FOLD in 0 1
+do
+    python ./ml/sum_training_weights.py ml/${ERA}_${CHANNEL}/fold${FOLD}_training_dataset.root $PROCESSES
+    echo
+done
+
+hadd -f ml/${ERA}_${CHANNEL}/combined_training_dataset.root \
+    ml/${ERA}_${CHANNEL}/fold0_training_dataset.root \
+    ml/${ERA}_${CHANNEL}/fold1_training_dataset.root
+echo
+python ./ml/sum_training_weights.py ml/${ERA}_${CHANNEL}/combined_training_dataset.root $PROCESSES
+echo
+
 PROCESSES="ggh qqh ztt noniso misc"
 CHANNEL=tt
 for FOLD in 0 1
