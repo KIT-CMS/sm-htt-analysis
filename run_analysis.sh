88f7882b8a9342d9d6347cf22c9a73238ce8cd33
Artur Gottmann <artur.gottmann@kit.edu>
2019-05-24 23:00:10 +0200

    run_analysis.sh & Co.: update the analysis chain to stage 1.1

--- run_analysis.sh
+++ run_analysis.sh
@@ -27,8 +27,8 @@ CHANNELS=${@:2}      # options: em, et, mt, tt
 ./shapes/convert_to_synced_shapes.sh $ERA
 
 # Write datacard
-STXS_SIGNALS="stxs_stage0"  # options: stxs_stage0, stxs_stage1
-CATEGORIES="stxs_stage1"    # options: stxs_stage0, stxs_stage1
+STXS_SIGNALS="stxs_stage0"  # options: stxs_stage0, stxs_stage1p1
+CATEGORIES="stxs_stage1p1"    # options: stxs_stage0, stxs_stage1p1
 JETFAKES=1                  # options: 0, 1
 EMBEDDING=1                 # options: 0, 1
 ./datacards/produce_datacard.sh $ERA $STXS_SIGNALS $CATEGORIES $JETFAKES $EMBEDDING $CHANNELS
@@ -40,7 +40,7 @@ EMBEDDING=1                 # options: 0, 1
 #./datacards/combine_datacards.sh 2016 2017
 
 # Build workspace
-STXS_FIT="inclusive"        # options: stxs_stage0, stxs_stage1, inclusive
+STXS_FIT="inclusive"        # options: stxs_stage0, stxs_stage1p1, inclusive
 ./datacards/produce_workspace.sh $ERA $STXS_FIT | tee ${ERA}_produce_workspace_${STXS_FIT}.log
 
 # Run statistical inference
