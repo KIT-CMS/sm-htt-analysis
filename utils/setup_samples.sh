adf949ddee5153be98647b7a4f4638a44f385e06
Sebastian Brommer <sebastian.brommer@student.kit.edu>
2019-06-06 11:26:23 +0200

    update Stage 1.1 shapes

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -3,13 +3,13 @@
 ERA=$1
 
 # Samples Run2016
-ARTUS_OUTPUTS_2016=/invalidceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/merged_fixedjets2
-ARTUS_FRIENDS_ET_2016=/invalidceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/et_keras_20190124
-ARTUS_FRIENDS_MT_2016=/invalidceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/mt_keras_20190124
-ARTUS_FRIENDS_TT_2016=/invalidceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/tt_keras_20190124
-ARTUS_FRIENDS_FAKE_FACTOR_2016=/invalidceph/swozniewski/SM_Htautau/ntuples/Artus16_Nov/Artus_2016_Jan19/fake_factor_friends_njets_mvis_NEW_NN_Jan27
-ARTUS_FRIENDS_FAKE_FACTOR_INCL_2016=$ARTUS_FRIENDS_FAKE_FACTOR_2016
-ARTUS_OUTPUTS_EM_2016=/invalidceph/jbechtel/2016/em_all_vars_JetFix_def
+ARTUS_OUTPUTS_2016=/storage/9/sbrommer/artus_outputs/2016_samples/2019_05_21
+ARTUS_FRIENDS_ET_2016=/storage/9/sbrommer/artus_outputs/2016_samples/2019_05_21_MELA_friends
+ARTUS_FRIENDS_MT_2016=/storage/9/sbrommer/artus_outputs/2016_samples/2019_05_21_MELA_friends
+ARTUS_FRIENDS_TT_2016=/storage/9/sbrommer/artus_outputs/2016_samples/2019_05_21_MELA_friends
+ARTUS_FRIENDS_FAKE_FACTOR_2016=/storage/9/sbrommer/artus_outputs/2016_samples/2019_05_21_fake_factor_friends
+ARTUS_FRIENDS_FAKE_FACTOR_INCL_2016=/storage/9/sbrommer/artus_outputs/2016_samples/2019_05_21_fake_factor_friends
+ARTUS_OUTPUTS_EM_2016=$ARTUS_OUTPUTS_2016
 
 
 # Samples Run2017
@@ -22,6 +22,7 @@ ARTUS_FRIENDS_TT_2017="/storage/b/akhmet/merged_files_from_naf/07-06-2019_full_a
 ARTUS_FRIENDS_EM_2017="/storage/b/akhmet/merged_files_from_naf/07-06-2019_full_analysis_MELA/MELA_collected/ /storage/b/akhmet/merged_files_from_naf/07-06-2019_full_analysis_SVFit/SVFit_collected/ ${NNScore}"
 ARTUS_FRIENDS_FAKE_FACTOR_2017="/storage/b/akhmet/merged_files_from_naf/07-06-2019_full_analysis_FakeFactors/FakeFactors_collected/"
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2017=$ARTUS_FRIENDS_FAKE_FACTOR_2017
+ARTUS_OUTPUTS_EM_2017=blank
 
 # Samples Run2018
 ARTUS_OUTPUTS_2018=/portal/ekpbms1/home/jbechtel/postprocessing/2018/sm-htt-analysis/files
@@ -45,6 +46,7 @@ then
 elif [[ $ERA == *"2017"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2017
+    ARTUS_OUTPUTS_EM=$ARTUS_OUTPUTS_EM_2017
     ARTUS_FRIENDS_ET=$ARTUS_FRIENDS_ET_2017
     ARTUS_FRIENDS_MT=$ARTUS_FRIENDS_MT_2017
     ARTUS_FRIENDS_TT=$ARTUS_FRIENDS_TT_2017
