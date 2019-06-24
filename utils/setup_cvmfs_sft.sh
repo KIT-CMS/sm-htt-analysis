c33d8c5ee05bd3f8c71acda1f0c8e52bae4145f2
Artur Gottmann <artur.gottmann@kit.edu>
2019-06-24 19:35:00 +0200

    utils/setup_cvmfs_sft.sh: bms1 is now a CentOS 7 machine

--- utils/setup_cvmfs_sft.sh
+++ utils/setup_cvmfs_sft.sh
@@ -9,6 +9,9 @@ then
 	export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
 	## This option enables locally installed packages and is needed for the ekpdeepthought, as tensorflow-gpu is not supplied by the lcga and needs to be installed locally.
 	#If this option is enabled on the bms123 by a user with tensorflow-gpu locally installed, the job fails due to a lack of cuda/gpus 
+elif uname -a | grep bms1
+then
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-centos7-gcc62-opt/setup.sh
 else
     source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-slc6-gcc62-opt/setup.sh
 fi
