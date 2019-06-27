2b441a3945612f68488c79807bdd51184e734b3f
Moritz Scham <moritz.scham@student.kit.edu>
2019-06-27 16:48:52 +0200

    ml/create_training_dataset.sh now uses multirun

--- ml/create_training_dataset.sh
+++ ml/create_training_dataset.sh
@@ -7,47 +7,67 @@ source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
 source utils/setup_samples.sh $ERA
 
-mkdir -p ml/${ERA}_${CHANNEL}
+function run_procedure() {
+    SELERA=$1
+    SELCHANNEL=$2
+    mkdir -p ml/${SELERA}_${SELCHANNEL}
 
-ARTUS_FRIENDS=""
-if [ ${CHANNEL} == 'mt' ]
-then
-    ARTUS_FRIENDS=${ARTUS_FRIENDS_MT}
-fi
-if [ ${CHANNEL} == 'et' ]
-then
-    ARTUS_FRIENDS=${ARTUS_FRIENDS_ET}
-fi
-if [ ${CHANNEL} == 'tt' ]
-then
-    ARTUS_FRIENDS=${ARTUS_FRIENDS_TT}
-fi
-if [ ${CHANNEL} == 'em' ]
-then
-    ARTUS_FRIENDS=${ARTUS_FRIENDS_EM}
-fi
+    ARTUS_FRIENDS=""
+    if [ ${SELCHANNEL} == 'mt' ]
+    then
+        ARTUS_FRIENDS=${ARTUS_FRIENDS_MT}
+    fi
+    if [ ${SELCHANNEL} == 'et' ]
+    then
+        ARTUS_FRIENDS=${ARTUS_FRIENDS_ET}
+    fi
+    if [ ${SELCHANNEL} == 'tt' ]
+    then
+        ARTUS_FRIENDS=${ARTUS_FRIENDS_TT}
+    fi
+    if [ ${SELCHANNEL} == 'em' ]
+    then
+        ARTUS_FRIENDS=${ARTUS_FRIENDS_EM}
+    fi
 
-# Write dataset config
-python ml/write_dataset_config.py \
-    --era ${ERA} \
-    --channel ${CHANNEL} \
-    --base-path $ARTUS_OUTPUTS \
-    --friend-paths $ARTUS_FRIENDS \
-    --database $KAPPA_DATABASE \
-    --output-path $PWD/ml/${ERA}_${CHANNEL} \
-    --output-filename training_dataset.root \
-    --tree-path ${CHANNEL}_nominal/ntuple \
-    --event-branch event \
-    --training-weight-branch training_weight \
-    --training-z-estimation-method emb \
-    --output-config ml/${ERA}_${CHANNEL}/dataset_config.yaml
+    # Write dataset config
+    python ml/write_dataset_config.py \
+        --era ${SELERA} \
+        --channel ${SELCHANNEL} \
+        --base-path $ARTUS_OUTPUTS \
+        --friend-paths $ARTUS_FRIENDS \
+        --database $KAPPA_DATABASE \
+        --output-path $PWD/ml/${SELERA}_${SELCHANNEL} \
+        --output-filename training_dataset.root \
+        --tree-path ${SELCHANNEL}_nominal/ntuple \
+        --event-branch event \
+        --training-weight-branch training_weight \
+        --training-z-estimation-method emb \
+        --output-config ml/${SELERA}_${SELCHANNEL}/dataset_config.yaml
 
-# Create dataset files from config
-./htt-ml/dataset/create_training_dataset.py ml/${ERA}_${CHANNEL}/dataset_config.yaml
+    # Create dataset files from config
+    ./htt-ml/dataset/create_training_dataset.py ml/${SELERA}_${SELCHANNEL}/dataset_config.yaml
 
-# Reweight STXS stage 1 signals so that each stage 1 signal is weighted equally but
-# conserve the overall weight of the stage 0 signal
-#python ml/reweight_stxs_stage1.py \
-#    ml/${ERA}_${CHANNEL} \
-#    ml/${ERA}_${CHANNEL}/fold0_training_dataset.root \
-#    ml/${ERA}_${CHANNEL}/fold1_training_dataset.root
+    # Reweight STXS stage 1 signals so that each stage 1 signal is weighted equally but
+    # conserve the overall weight of the stage 0 signal
+    #python ml/reweight_stxs_stage1.py \
+    #    ml/${SELERA}_${SELCHANNEL} \
+    #    ml/${SELERA}_${SELCHANNEL}/fold0_training_dataset.root \
+    #    ml/${SELERA}_${SELCHANNEL}/fold1_training_dataset.root
+}
+
+CHANNELS=$(if [[ -z $CHANNEL ]]; then echo "tt" "mt" "et"; else echo $CHANNEL; fi)
+ERAS=$(if [[ -z $ERA ]]; then echo "2016" "2017"; else echo $ERA; fi)
+
+i=0 ## how many sets of arguments
+unset argslist
+for era in $ERAS
+do
+    for channel in $CHANNELS;do
+        argslist[$i]=" $era $channel"
+        i=$(($i+1))
+    done
+done
+
+source utils/multirun.sh
+multirun run_procedure ${argslist[@]}
