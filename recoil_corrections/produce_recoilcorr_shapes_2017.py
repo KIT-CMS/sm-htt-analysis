e1d461c399f9e8dc373567505922d52baabdf351
Artur Gottmann <artur.gottmann@kit.edu>
2019-07-12 12:20:25 +0200

    recoil_corrections: update workflow

--- recoil_corrections/produce_recoilcorr_shapes_2017.py
+++ recoil_corrections/produce_recoilcorr_shapes_2017.py
@@ -40,7 +40,7 @@ def setup_logging(output_file, level=logging.DEBUG):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description="Produce shapes for 2016 Standard Model analysis.")
+        description="Produce shapes for 2017 Standard Model analysis.")
 
     parser.add_argument(
         "--directory",
@@ -70,11 +70,12 @@ def main(args):
     # yapf: disable
     directory = args.directory
 
+    zptm_path = "/portal/ekpbms1/home/akhmet/workdir/FriendTreeProductionMain/CMSSW_10_2_14/src/ZPtMReweighting_workdir/ZPtMReweighting_collected/"
     mm = MM()
     mm_processes = {
         "data"  : Process("data_obs", DataEstimation      (era, directory, mm, friend_directory=[])),
-        "ZTT"   : Process("ZTT",      ZTTEstimation       (era, directory, mm, friend_directory=[])),
-        "ZL"    : Process("ZL",       ZLEstimation        (era, directory, mm, friend_directory=[])),
+        "ZTT"   : Process("ZTT",      ZTTEstimation       (era, directory, mm, friend_directory=[zptm_path])),
+        "ZL"    : Process("ZL",       ZLEstimation        (era, directory, mm, friend_directory=[zptm_path])),
         "TTT"   : Process("TTT",      TTTEstimation       (era, directory, mm, friend_directory=[])),
         "TTL"   : Process("TTL",      TTLEstimation       (era, directory, mm, friend_directory=[])),
         "VVT"   : Process("VVT",      VVTEstimation       (era, directory, mm, friend_directory=[])),
@@ -145,5 +146,5 @@ def main(args):
 
 if __name__ == "__main__":
     args = parse_arguments()
-    setup_logging("produce_shapes.log", logging.INFO)
+    setup_logging("produce_shapes_recoil_2017.log", logging.INFO)
     main(args)
