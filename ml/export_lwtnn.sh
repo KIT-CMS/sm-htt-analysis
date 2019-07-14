bb655e99412101b2eda26c134f4aaa626c6e2dc9
Moritz Scham <moritz.scham@student.kit.edu>
2019-07-15 00:22:57 +0200

    export_lwtnn: bms1 fix

--- ml/export_lwtnn.sh
+++ ml/export_lwtnn.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+set -e
 
 ERA=$1
 CHANNEL=$2
 
 # source python3 LCG view
-source /cvmfs/sft.cern.ch/lcg/views/LCG_94python3/x86_64-slc6-gcc62-opt/setup.sh
+LCG_RELEASE=94python3
+if uname -a | grep ekpdeepthought
+then
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-ubuntu1604-gcc54-opt/setup.sh
+elif uname -a | grep bms1
+then
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-centos7-gcc62-opt/setup.sh
+else
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-slc6-gcc62-opt/setup.sh
+fi
 
 if [ ! -d "htt-ml/lwtnn" ]; then
     echo "[FATAL] Directory htt-ml/lwtnn not found, please checkout this submodule of htt-ml"
@@ -18,3 +28,4 @@ done
 
 echo "Created lwtnn .json files:"
 ls ml/${ERA}_${CHANNEL}/*lwtnn.json -lrth
+exit $!
