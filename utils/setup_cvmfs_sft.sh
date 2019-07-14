74ecc634afc981a6d75c2c74aa78d647e769ac34
Moritz Scham <moritz.scham@student.kit.edu>
2019-07-15 00:18:51 +0200

    setup_samples: use /ceph/htautau setup_cuda: host specific local python

--- utils/setup_cvmfs_sft.sh
+++ utils/setup_cvmfs_sft.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 LCG_RELEASE=94
-
 if uname -a | grep ekpdeepthought
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-ubuntu1604-gcc54-opt/setup.sh
-
-	export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
-	## This option enables locally installed packages and is needed for the ekpdeepthought, as tensorflow-gpu is not supplied by the lcga and needs to be installed locally.
-	#If this option is enabled on the bms123 by a user with tensorflow-gpu locally installed, the job fails due to a lack of cuda/gpus 
 elif uname -a | grep bms1
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-centos7-gcc62-opt/setup.sh
