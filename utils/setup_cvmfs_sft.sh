0520a41cb3604b04bb8f1668f9bb7e105fdc459e
Artur Gottmann <artur.gottmann@kit.edu>
2019-08-05 23:15:36 +0200

    utils/setup_cvmfs_sft.sh: update LCG source script

--- utils/setup_cvmfs_sft.sh
+++ utils/setup_cvmfs_sft.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LCG_RELEASE=95
-if uname -a | grep ekpdeepthought
+if uname -a | grep ekpdeepthought -q
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-ubuntu1604-gcc54-opt/setup.sh
     source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-ubuntu1604-gcc54-opt/setup.sh
-elif uname -a | grep bms1
+elif uname -a | grep -E 'bms1|sg0|sm0' -q
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-centos7-gcc8-opt/setup.sh
 else
