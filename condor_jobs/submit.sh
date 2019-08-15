aa597963ccdc234ce8dfec28d57e4e7205f8867a
Sebastian Brommer <sebastian.brommer@kit.edu>
2019-08-15 17:02:46 +0200

    Update submit.sh

--- condor_jobs/submit.sh
+++ condor_jobs/submit.sh
@@ -21,11 +21,11 @@ done > arguments.txt
 if uname -a | grep -E 'bms1|ams2|sg0|sm0' -q
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/setup.sh
-    #condor_submit produce_shapes_cc7.jdl
+    condor_submit produce_shapes_cc7.jdl
 elif uname -a | grep -E 'bms2|bms3|cms6' -q
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc8-opt/setup.sh
-    #condor_submit produce_shapes_slc6.jdl
+    condor_submit produce_shapes_slc6.jdl
 else
     echo "Maschine unknown, i don't know what to do !"
 fi
