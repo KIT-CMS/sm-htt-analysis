e63afd025a980654c8a27dc3cdd38b0b4cb7f0a8
Artur Gottmann <artur.gottmann@kit.edu>
2019-08-17 19:47:13 +0200

    cutbased_shapes/convert_to_synced_shapes.*: enable multi-processed conversion

--- cutbased_shapes/convert_to_synced_shapes.sh
+++ cutbased_shapes/convert_to_synced_shapes.sh
@@ -2,8 +2,9 @@
 
 ERA=$1
 VARIABLE=$2
+NUM_CORES=35
 
 source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
 
-python cutbased_shapes/convert_to_synced_shapes.py ${ERA} ${VARIABLE} ${ERA}_cutbased_shapes_${VARIABLE}.root .
+python cutbased_shapes/convert_to_synced_shapes.py ${NUM_CORES} ${ERA} ${VARIABLE} ${ERA}_cutbased_shapes_${VARIABLE}.root ${PWD}
