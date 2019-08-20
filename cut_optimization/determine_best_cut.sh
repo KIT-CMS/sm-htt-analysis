134528ac7a10ce8303a57d488839ac824c939365
Artur Gottmann <artur.gottmann@kit.edu>
2019-08-20 19:12:05 +0200

    cut_optimization: make cut scan plots from splitted count files

--- cut_optimization/determine_best_cut.sh
+++ cut_optimization/determine_best_cut.sh
@@ -1,4 +1,7 @@
 source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
 
-python cut_optimization/determine_best_cut.py
+for f in 2017_??_counts_for_soverb_jdeta.root;
+do
+    python cut_optimization/determine_best_cut.py ${f}
+done
