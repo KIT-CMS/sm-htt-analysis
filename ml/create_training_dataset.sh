fd20e90e50dfc01ee4e0d13a2df8d038a2b6be7d
Moritz Scham <moritz.scham@student.kit.edu>
2019-09-05 15:24:10 +0200

    ml/create_training_dataset.sh sum_weights -> yamlmerge.py

--- ml/create_training_dataset.sh
+++ ml/create_training_dataset.sh
@@ -13,14 +13,12 @@ source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
 source utils/bashFunctionCollection.sh
 
-if [[ $method == *"emb"* ]]; then
-    tauEstimation=emb
-else
+tauEstimation=emb
+jetEstimation=ff
+if [[ $method == *"mc_"* ]]; then
     tauEstimation=mc
 fi
-if [[ $method == *"ff"* ]]; then
-    jetEstimation=ff
-else
+if [[ $method == *"_mc"* ]]; then
     jetEstimation=mc
 fi
 
@@ -49,43 +47,39 @@ function run_procedure() {
         ARTUS_FRIENDS=${ARTUS_FRIENDS_EM}
     fi
     # Write dataset config
-    logandrun python ml/write_dataset_config.py \
-        --era ${SELERA} \
-        --channel ${SELCHANNEL} \
-        --base-path $ARTUS_OUTPUTS \
-        --friend-paths $ARTUS_FRIENDS \
-        --database $KAPPA_DATABASE \
-        --output-path $outdir \
-        --output-filename training_dataset.root \
-        --tree-path ${SELCHANNEL}_nominal/ntuple \
-        --event-branch event \
-        --training-weight-branch training_weight \
-        --training-z-estimation-method $tauEstimation \
-        --training-jetfakes-estimation-method $jetEstimation \
-        --output-config $outdir/dataset_config.yaml
+     logandrun python ml/write_dataset_config.py \
+         --era ${SELERA} \
+         --channel ${SELCHANNEL} \
+         --base-path $ARTUS_OUTPUTS \
+         --friend-paths $ARTUS_FRIENDS \
+         --database $KAPPA_DATABASE \
+         --output-path $outdir \
+         --output-filename training_dataset.root \
+         --tree-path ${SELCHANNEL}_nominal/ntuple \
+         --event-branch event \
+         --training-weight-branch training_weight \
+         --training-z-estimation-method $tauEstimation \
+         --training-jetfakes-estimation-method $jetEstimation \
+         --output-config $outdir/dataset_config.yaml
 
-    # # Create dataset files from config
-    logandrun ./htt-ml/dataset/create_training_dataset.py $outdir/dataset_config.yaml
-    # Reweight STXS stage 1 signals so that each stage 1 signal is weighted equally but
-    # conserve the overall weight of the stage 0 signal
-    #python ml/reweight_stxs_stage1.py \
-    #    ml/${SELERA}_${SELCHANNEL} \
-    #    ml/${SELERA}_${SELCHANNEL}/fold0_training_dataset.root \
-    #    ml/${SELERA}_${SELCHANNEL}/fold1_training_dataset.root
-    # split the dataset
-    logandrun hadd -f $outdir/combined_training_dataset.root \
-        $outdir/fold0_training_dataset.root \
-        $outdir/fold1_training_dataset.root
+     # # Create dataset files from config
+     logandrun ./htt-ml/dataset/create_training_dataset.py $outdir/dataset_config.yaml
+     # Reweight STXS stage 1 signals so that each stage 1 signal is weighted equally but
+     # conserve the overall weight of the stage 0 signal
+     #python ml/reweight_stxs_stage1.py \
+     #    $outdir \
+     #    $outdir/fold0_training_dataset.root \
+     #    $outdir/fold1_training_dataset.root
+     # split the dataset
+     logandrun hadd -f $outdir/combined_training_dataset.root \
+         $outdir/fold0_training_dataset.root \
+         $outdir/fold1_training_dataset.root
 
     # write the classweight to dataset_config.yaml
-    logandrun python ./ml/sum_training_weights.py \
+    logandrun python ./ml/yamlmerge.py \
         --era ${SELERA} \
         --channel ${SELCHANNEL} \
-        --dataset $outdir/combined_training_dataset.root \
-        --dataset-config-file "$outdir/dataset_config.yaml" \
-        --training-template "ml/templates/${SELERA}_${SELCHANNEL}_training.yaml" \
-        --channel $SELCHANNEL \
-        --write-weights True
+        --dataset-config-file ml/out/${SELERA}_${SELCHANNEL}_${method}/dataset_config.yaml
 }
 source utils/multirun.sh
 genArgsAndRun run_procedure $@
