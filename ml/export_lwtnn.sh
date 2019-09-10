ecf63f3581661d3dbb8a5645e1c5b01367b509d3
Sebastian Brommer <sebastian.brommer@kit.edu>
2019-09-10 12:46:40 +0200

    Update export_lwtnn.sh
    fix bash logic

--- ml/export_lwtnn.sh
+++ ml/export_lwtnn.sh
@@ -9,7 +9,7 @@ LCG_RELEASE=94python3
 if uname -a | grep ekpdeepthought
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-ubuntu1604-gcc54-opt/setup.sh
-if uname -a | grep -E 'el7' -q
+elif uname -a | grep -E 'el7' -q
 then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-centos7-gcc62-opt/setup.sh
 else
