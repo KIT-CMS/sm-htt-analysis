1a7c9e39ebdc49c3e4d34063aa8a10812a61f44c
janekbechtel <janek.bechtel@student.kit.edu>
2019-11-30 14:44:10 +0100

    Update sample path

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -28,20 +28,20 @@ ARTUS_FRIENDS_FAKE_FACTOR_INCL_2016=$ARTUS_FRIENDS_FAKE_FACTOR_2016
 
 
 # Samples Run2017
-basedir="/ceph/htautau"
+basedir="/ceph/htautau/deeptau"
 ARTUS_OUTPUTS_2017="$basedir/2017/ntuples/"
-NNScore_Friends_2017="$basedir/2017/nnscore_friends/"
-SVFit_Friends_2017="$basedir/2017/svfit_friends/"
-MELA_Friends_2017="$basedir/2017/mela_friends/"
-FF_Friends_2017="$basedir/2017/ff_friends/"
+NNScore_Friends_2017="$basedir/2017/friends/NNScore/"
+SVFit_Friends_2017="$basedir/2017/friends/SVFit/"
+MELA_Friends_2017="$basedir/2017/friends/MELA/"
+FF_Friends_2017="$basedir/2017/friends/FakeFactors/"
 if [[ -d output/friend_trees ]]; then
-    DIR=${WD}/output/friend_trees/2017/nnscore_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && NNScore_Friends_2017=$DIR
-    DIR=${WD}/output/friend_trees/2017/svfit_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && SVFit_Friends_2017=$DIR
-    DIR=${WD}/output/friend_trees/2017/mela_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && MELA_Friends_2017=$DIR
-    DIR=${WD}/output/friend_trees/2017/ff_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && FF_Friends_2017=$DIR
+    DIR=${WD}/output/friend_trees/2017/friends/NNScore/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && NNScore_Friends_2017=$DIR
+    DIR=${WD}/output/friend_trees/2017/friends/SVFit/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && SVFit_Friends_2017=$DIR
+    DIR=${WD}/output/friend_trees/2017/friends/MELA/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && MELA_Friends_2017=$DIR
+    DIR=${WD}/output/friend_trees/2017/friends/FakeFactors/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && FF_Friends_2017=$DIR
 fi
 
-ARTUS_FRIENDS_2017="$NNScore_Friends_2017 $SVFit_Friends_2017 $MELA_Friends_2017"
+ARTUS_FRIENDS_2017="$SVFit_Friends_2017 $MELA_Friends_2017 $NNScore_Friends_2017"
 ARTUS_FRIENDS_ET_2017=$ARTUS_FRIENDS_2017
 ARTUS_FRIENDS_MT_2017=$ARTUS_FRIENDS_2017
 ARTUS_FRIENDS_TT_2017=$ARTUS_FRIENDS_2017
@@ -51,27 +51,23 @@ ARTUS_FRIENDS_FAKE_FACTOR_INCL_2017=$FF_Friends_2017
 ARTUS_OUTPUTS_EM_2017=""
 
 # Samples Run2018
-basedir="/ceph/htautau"
+basedir="/ceph/htautau/deeptau"
 ARTUS_OUTPUTS_2018="$basedir/2018/ntuples/"
-NNScore_Friends_2018=""
-NNScore_Friends_ET_2018="/ceph/jbechtel/artus_outputs/2018/friends/nn_scores_naf/et/"
-NNScore_Friends_MT_2018="/ceph/jbechtel/artus_outputs/2018/friends/nn_scores_naf/mt/"
-NNScore_Friends_TT_2018="/ceph/jbechtel/artus_outputs/2018/friends/nn_scores_naf/tt/"
-NNScore_Friends_EM_2018="/ceph/jbechtel/artus_outputs/2018/friends/nn_scores_naf/em/"
-SVFit_Friends_2018="$basedir/2018/svfit_friends/"
-MELA_Friends_2018="$basedir/2018/mela_friends/"
-FF_Friends_2018="$basedir/2018/ff_friends/"
+NNScore_Friends_2018="$basedir/2018/friends/NNScore/"
+SVFit_Friends_2018="$basedir/2018/friends/SVFit/"
+MELA_Friends_2018="$basedir/2018/friends/MELA/"
+FF_Friends_2018="$basedir/2018/friends/FakeFactors/"
 if [[ -d output/friend_trees ]]; then
-    DIR=${WD}/output/friend_trees/2018/nnscore_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && NNScore_Friends_2018=$DIR
-    DIR=${WD}/output/friend_trees/2018/svfit_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && SVFit_Friends_2018=$DIR
-    DIR=${WD}/output/friend_trees/2018/mela_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && MELA_Friends_2018=$DIR
-    DIR=${WD}/output/friend_trees/2018/ff_friends/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && FF_Friends_2018=$DIR
+    DIR=${WD}/output/friend_trees/2018/friends/NNScore/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && NNScore_Friends_2017=$DIR
+    DIR=${WD}/output/friend_trees/2018/friends/SVFit/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && SVFit_Friends_2017=$DIR
+    DIR=${WD}/output/friend_trees/2018/friends/MELA/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && MELA_Friends_2017=$DIR
+    DIR=${WD}/output/friend_trees/2018/friends/FakeFactors/${TAG}/ && [[ -d $DIR && $(ls -A $DIR | wc -l ) -gt 5 ]] && FF_Friends_2017=$DIR
 fi
 ARTUS_FRIENDS_2018="$SVFit_Friends_2018 $MELA_Friends_2018 $NNScore_Friends_2018"
-ARTUS_FRIENDS_ET_2018="$SVFit_Friends_2018 $MELA_Friends_2018 $NNScore_Friends_ET_2018"
-ARTUS_FRIENDS_MT_2018="$SVFit_Friends_2018 $MELA_Friends_2018 $NNScore_Friends_MT_2018"
-ARTUS_FRIENDS_TT_2018="$SVFit_Friends_2018 $MELA_Friends_2018 $NNScore_Friends_TT_2018"
-ARTUS_FRIENDS_EM_2018="$SVFit_Friends_2018 $MELA_Friends_2018 $NNScore_Friends_EM_2018"
+ARTUS_FRIENDS_ET_2018="$SVFit_Friends_2018"
+ARTUS_FRIENDS_MT_2018="$SVFit_Friends_2018"
+ARTUS_FRIENDS_TT_2018="$SVFit_Friends_2018"
+ARTUS_FRIENDS_EM_2018="$SVFit_Friends_2018"
 ARTUS_FRIENDS_FAKE_FACTOR_2018=$FF_Friends_2018
 ARTUS_FRIENDS_FAKE_FACTOR_INCL_2018=$FF_Friends_2018
 
