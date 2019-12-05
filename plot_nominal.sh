20313e71cf9cc5ab86b4765a5c1521ce62060de9
janekbechtel <janek.bechtel@student.kit.edu>
2019-12-05 10:40:54 +0100

    Adapt plot nominal script to new shapes path and checkout shape producer

--- plot_nominal.sh
+++ plot_nominal.sh
@@ -4,8 +4,10 @@ VARIABLE=$3
 
 ./shapes/produce_shapes_variables_nominal.sh $ERA $CHANNEL $VARIABLE 
 
-python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes ${ERA}_${CHANNEL}_${VARIABLE}_shapes.root -c $CHANNEL --emb --comparison &
-python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes ${ERA}_${CHANNEL}_${VARIABLE}_shapes.root -c $CHANNEL --emb  &
-python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes ${ERA}_${CHANNEL}_${VARIABLE}_shapes.root -c $CHANNEL  &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL --emb --ff &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL --emb  &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL --ff  &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL  &
+
 wait
 
