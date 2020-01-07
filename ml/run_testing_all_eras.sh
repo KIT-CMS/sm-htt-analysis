35eb813df189eedb2aa4f2c399f77db71837206f
sijoerger <simon.joerger@student.kit.edu>
2020-01-07 10:48:01 +0100

    Bug fixes in run_testing_all_eras.sh

--- ml/run_testing_all_eras.sh
+++ ml/run_testing_all_eras.sh
@@ -5,6 +5,7 @@ CHANNEL=$2
 
 source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
+source utils/bashFunctionCollection.sh
 
 export KERAS_BACKEND=tensorflow
 export OMP_NUM_THREADS=12
@@ -28,10 +29,10 @@ mkdir -p ${outdir}
 # Confusion matrices
 TEST_CONFUSION_MATRIX=1
 if [ -n "$TEST_CONFUSION_MATRIX" ]; then
-python htt-ml/testing/keras_confusion_matrix.py \
+logandrun python htt-ml/testing/keras_confusion_matrix.py \
     ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${CHANNEL}.yaml 0 --era $ERA
 
-python htt-ml/testing/keras_confusion_matrix.py \
+logandrun python htt-ml/testing/keras_confusion_matrix.py \
     ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${CHANNEL}.yaml 1 --era $ERA
 fi
 
@@ -39,10 +40,10 @@ fi
 export KERAS_BACKEND=tensorflow
 TEST_TAYLOR_1D=1
 if [ -n "$TEST_TAYLOR_1D" ]; then
-python htt-ml/testing/keras_taylor_1D.py \
+logandrun python htt-ml/testing/keras_taylor_1D.py \
     ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${CHANNEL}.yaml 0 --era $ERA
 
-python htt-ml/testing/keras_taylor_1D.py \
+logandrun python htt-ml/testing/keras_taylor_1D.py \
     ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${CHANNEL}.yaml 1 --era $ERA
 fi
 
@@ -51,8 +52,8 @@ export KERAS_BACKEND=tensorflow
 #TEST_TAYLOR_RANKING=1
 if [ -n "$TEST_TAYLOR_RANKING" ]; then
 logandrun python htt-ml/testing/keras_taylor_ranking.py \
-    ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${channel}_testing.yaml 0
+    ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${CHANNEL}.yaml 0 --era $ERA
 
 logandrun python htt-ml/testing/keras_taylor_ranking.py \
-    ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${channel}_testing.yaml 1
+    ${outdir}/dataset_config.yaml ml/templates/all_eras_testing_${CHANNEL}.yaml 1 --era $ERA
 fi
