e91778dc47f39746d072212d0ec97dfb929f20ce
mova <mova@users.noreply.github.com>
2020-01-10 10:48:46 +0100

    utils/setup_samples.sh: read from ceph again

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -11,27 +11,25 @@ KAPPA_DATABASE=datasets/datasets.json
 #### ERA specific part. If a sample is not available comment it out here.
 # Samples Run2016
 basedir="/ceph/htautau/deeptau"
-ARTUS_OUTPUTS_2016="/portal/ekpbms2/home/jbechtel/deeptau/2016/ntuples/"
+ARTUS_OUTPUTS_2016="$basedir/2016/ntuples/"
 #NNScore_Friends_2016="$basedir/2016/friends/NNScore/"
-SVFit_Friends_2016="/portal/ekpbms2/home/jbechtel/deeptau/2016/friends/SVFit/"
-MELA_Friends_2016="/portal/ekpbms2/home/jbechtel/deeptau/2016/friends/MELA/"
-FF_Friends_2016="/portal/ekpbms2/home/jbechtel/deeptau/2016/friends/FakeFactors/"
+SVFit_Friends_2016="$basedir/2016/friends/SVFit/"
+MELA_Friends_2016="$basedir/2016/friends/MELA/"
+FF_Friends_2016="$basedir/2016/friends/FakeFactors/"
 
 # Samples Run2017
-basedir="/ceph/htautau/deeptau"
 ARTUS_OUTPUTS_2017="$basedir/2017/ntuples/"
-#NNScore_Friends_2017=""
+#NNScore_Friends_2017="$basedir/2017/friends/NNScore/"
 SVFit_Friends_2017="$basedir/2017/friends/SVFit/"
 MELA_Friends_2017="$basedir/2017/friends/MELA/"
 FF_Friends_2017="$basedir/2017/friends/FakeFactors/"
 
 # Samples Run2018
-basedir="/ceph/htautau/deeptau"
-ARTUS_OUTPUTS_2018="/portal/ekpbms3/home/jbechtel/deeptau/2018/ntuples/"
-NNScore_Friends_2018="/portal/ekpbms3/home/jbechtel/deeptau/2018/friends/NNScore/"
-SVFit_Friends_2018="/portal/ekpbms3/home/jbechtel/deeptau/2018/friends/SVFit/"
-MELA_Friends_2018="/portal/ekpbms3/home/jbechtel/deeptau/2018/friends/MELA/"
-FF_Friends_2018="/portal/ekpbms3/home/jbechtel/deeptau/2018/friends/FakeFactors/"
+ARTUS_OUTPUTS_2018="$basedir/2018/ntuples/"
+#NNScore_Friends_2018="$basedir/2018/friends/NNScore/"
+SVFit_Friends_2018="$basedir/2018/friends/SVFit/"
+MELA_Friends_2018="$basedir/2018/friends/MELA/"
+FF_Friends_2018="$basedir/2018/friends/FakeFactors/"
 
 
 # ERA handling
