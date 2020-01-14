dcf63dd7ac7e92975057037061e092150aacef64
mova <mova@users.noreply.github.com>
2020-01-14 09:08:54 +0100

    set switches for mc and stage0 in datacards/produce_datacard.sh

--- datacards/produce_datacard.sh
+++ datacards/produce_datacard.sh
@@ -18,7 +18,16 @@ NUM_THREADS=8
 
 TRAIN_EMB=1
 TRAIN_FF=1
-
+if [[ $TAG == *"mc_"* ]]; then
+    TRAIN_EMB=0
+fi
+if [[ $TAG == *"_mc"* ]]; then
+    TRAIN_FF=0
+fi
+TRAIN_STAGE0=0
+if [[ $TAG == *"stage0"* ]]; then
+    TRAIN_STAGE0=1
+fi
 OUTPUTDIR="output/datacards/${ERA}-${TAG}-smhtt-ML/${STXS_SIGNALS}"
 
 # Remove output directory
@@ -47,6 +56,7 @@ logandrun ${CMSSW_BASE}/bin/slc7_amd64_gcc700/MorphingSMRun2Legacy \
     --era=${ERA} \
     --output=${OUTPUTDIR} \
     --train_ff=${TRAIN_FF} \
+    --train_stage0=${TRAIN_STAGE0} \
     --train_emb=${TRAIN_EMB} | tee output/log/datacard-${ERA}-${STXS_SIGNALS}-${CATEGORIES}-${JETFAKES}-${EMBEDDING}-${TAG}-${CHANNELS}.log
 
 # Use Barlow-Beeston-lite approach for bin-by-bin systematics
