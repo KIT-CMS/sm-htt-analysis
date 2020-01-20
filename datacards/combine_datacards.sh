18949d2509c705f1a2a9ddb3273207e1cfdda0f2
mova <mova@users.noreply.github.com>
2020-01-20 16:48:01 +0100

    combine_datacards.sh fix remove previously added eras

--- datacards/combine_datacards.sh
+++ datacards/combine_datacards.sh
@@ -11,16 +11,18 @@ elif [[ $STXS_FIT == "stxs_stage1p1" ]] ; then
     STXS_SIGNALS=stxs_stage1p1
 fi
 
+set -x
 
-for ERA in ${ERAS[@]}; do
+for STXS_SIGNALS in "stxs_stage0" "stxs_stage1p1"; do
     for CHANNEL in ${CHANNELS[@]}; do
-        for STXS_SIGNALS in "stxs_stage0" "stxs_stage1p1"; do
+        TARGET=output/datacards/all-comberas-smhtt-ML/${STXS_SIGNALS}/$CHANNEL
+        ## remove old files
+        ls $TARGET/125/htt_*_Run*.txt &> /dev/null && rm $TARGET/125/htt_*_Run*.txt
+        [[ -f $TARGET/common/htt_input_Run*.root ]] && rm $TARGET/common/htt_input_Run*.root
+        for ERA in ${ERAS[@]}; do
             DATACARDDIR=output/datacards/${ERA}-${TAG}-smhtt-ML/${STXS_SIGNALS}/$CHANNEL
-            TARGET=output/datacards/all-comberas-smhtt-ML/${STXS_SIGNALS}/$CHANNEL
-            [[ -d $TARGET ]] && rm -r $TARGET
             # Make new directory with needed folder structure
             mkdir -p $TARGET/{125,common}
-
             cp ${DATACARDDIR}/125/htt_*_Run${ERA}.txt $TARGET/125
             cp ${DATACARDDIR}/common/htt_input_Run${ERA}.root $TARGET/common
         done
