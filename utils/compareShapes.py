d86411926caa3e1eb79e79f0562be1a87120c398
mova <mova@users.noreply.github.com>
2020-01-22 10:49:47 +0100

    compareShapes better formatting

--- utils/compareShapes.py
+++ utils/compareShapes.py
@@ -1,29 +1,35 @@
+#!/usr/bin/env python
 # %%
 import sys,os
 import uproot
-import numpy as np
+#  import numpy as np
 
 # %%
-p1=sys.argv[0]
-p2=sys.argv[1]
-shape1 = uproot.open(os.path.expanduser(p1))
-shape2 = uproot.open(os.path.expanduser(p2))
+p1=sys.argv[1]
+p2=sys.argv[2]
+print(sys.argv)
+shape1 = uproot.open(p1)
+shape2 = uproot.open(p2)
+
+GREEN='\x1b[6;30;42m'
+RED='\x1b[0;30;41m'
+END='\x1b[0m'
 
 # %%
 print("{} | {}".format(p1,p2))
-print("<: key in {} but not in {} ".format(p1,p2))
-print(">: key in {} but not in {} ".format(p2,p1))
-diffkeys = set(shape1.allkeys()) ^ set(shape2.allkeys())
-samekeys = set(shape1.allkeys()) | set(shape2.allkeys())
+print(GREEN+">"+END+": key in {} but not in {} ".format(p1,p2))
+print(RED+"<"+END+": key in {} but not in {} ".format(p2,p1))
+diffkeys = sorted(set(shape1.allkeys()) ^ set(shape2.allkeys()))
+samekeys = sorted(set(shape1.allkeys()) & set(shape2.allkeys()))
 if len(diffkeys)==0:
     print("Keys are the same")
 else:
     for key in diffkeys:
         if key in shape1.allkeys():
-            print("{key} <".format(key=key))
+            print(GREEN+">"+END+" {key} >".format(key=key))
         else:
-            print("{key} >".format(key=key))
-
+            print(RED+"<"+END+" {key} <".format(key=key))
+{} + []
 # %%
 print("Analysing Histogramm content")
 for key in samekeys:
@@ -31,7 +37,6 @@ for key in samekeys:
     arr2,bin2=shape2[key].allnumpy()
     if any(arr1==arr2 ) or any(bin1!=bin2):
         if any(bin1!=bin2):
-            print("{} \n binning different: \n {} \n vs. \n {}".format(key, bin1, bin2))
             print("{} \n binning different: \n {} \n vs. \n {}".format(key, bin1, bin2))
         else:
             print("{} \n contents different: \n {} \n vs. \n {}".format(key, arr1, arr2))
