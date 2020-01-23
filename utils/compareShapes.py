c88ee8f874fd89657a2d62812c8cac31622d9fc9
mova <mova@users.noreply.github.com>
2020-01-23 15:40:48 +0100

    compareShapes add set of changes to ignore

--- utils/compareShapes.py
+++ utils/compareShapes.py
@@ -2,7 +2,7 @@
 # %%
 import sys,os
 import uproot
-#  import numpy as np
+import numpy as np
 
 # %%
 p1=sys.argv[1]
@@ -19,24 +19,43 @@ END='\x1b[0m'
 print("{} | {}".format(p1,p2))
 print(GREEN+">"+END+": key in {} but not in {} ".format(p1,p2))
 print(RED+"<"+END+": key in {} but not in {} ".format(p2,p1))
-diffkeys = sorted(set(shape1.allkeys()) ^ set(shape2.allkeys()))
-samekeys = sorted(set(shape1.allkeys()) & set(shape2.allkeys()))
+
+keyset1=shape1.allkeys()
+keyset2=shape2.allkeys()
+ignoreS={"CMS_htt_dyShape"}
+ignoreS= ignoreS | {"CMS_scale_mc_t_1prong", "CMS_scale_mc_t_3prong"} # mc_tau energy scale added
+# ignoreS= ignoreS | {"CMS_eff_t_dm11","CMS_eff_emb_t_dm11"} ##dm11 fixed
+# ignoreS= ignoreS | {"Down;"} # ignore down shifts
+#{"CMS_eff_emb_t_","CMS_eff_t_","CMS_scale_mc_t_"}
+
+keyset1 = {key.replace("data_obs","data") for key in keyset1 if not any([pattern in key for pattern in ignoreS]) }
+keyset2 = {key.replace("data_obs","data") for key in keyset2 if not any([pattern in key for pattern in ignoreS]) }
+
+diffkeys = sorted(set(keyset1) ^ set(keyset2))
+
 if len(diffkeys)==0:
     print("Keys are the same")
 else:
     for key in diffkeys:
-        if key in shape1.allkeys():
+        if key in keyset1:
             print(GREEN+">"+END+" {key} >".format(key=key))
         else:
             print(RED+"<"+END+" {key} <".format(key=key))
-{} + []
+
 # %%
+exit(0)
+keyset1=shape1.allkeys()
+keyset2=shape2.allkeys()
+samekeys = sorted(set(keyset1) & set(keyset2))
+
 print("Analysing Histogramm content")
 for key in samekeys:
     arr1,bin1=shape1[key].allnumpy()
     arr2,bin2=shape2[key].allnumpy()
-    if any(arr1==arr2 ) or any(bin1!=bin2):
-        if any(bin1!=bin2):
+    arr1=np.round(arr1,decimals=4)
+    arr2=np.round(arr2,decimals=4)
+    if not np.array_equal(arr1,arr2) or not np.array_equal(bin1,bin2):
+        if not np.array_equal(bin1,bin2):
             print("{} \n binning different: \n {} \n vs. \n {}".format(key, bin1, bin2))
         else:
             print("{} \n contents different: \n {} \n vs. \n {}".format(key, arr1, arr2))
