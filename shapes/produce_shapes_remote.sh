07a0d71ffd9f5f18e46849b7ecc34010e3293b07
Sebastian Brommer <sebastian.brommer@kit.edu>
2020-01-23 09:53:42 +0100

    add feature to limit process und category for shape production (#38)
    * add feature to limit process und category for shape production
    
    * update output file location

---
+++ shapes/produce_shapes_remote.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+ERA=$1
+CHANNELS=$2
+TAG=$3
+CATGEGORIES=$4
+PROCESSES=$5
+[[ ! -z $6 ]] && PWD=$6 || PWD=$( pwd  )
+BINNING=shapes/binning.yaml
+cd $PWD
+
+export LCG_RELEASE=96
+source utils/setup_cvmfs_sft.sh
+
+source utils/setup_python.sh
+source utils/setup_samples.sh $ERA $TAG
+source utils/bashFunctionCollection.sh
+ensureoutdirs
+
+# Produce shapes
+logandrun python shapes/produce_shapes_joined.py \
+    --tag ${TAG} \
+    --era ${ERA} \
+    --channels ${CHANNELS} \
+    --processes ${PROCESSES} \
+    --categories ${CATGEGORIES} \
+    --datasets $KAPPA_DATABASE \
+    --binning $BINNING \
+    --num-threads 1 \
+    --directory $ARTUS_OUTPUTS \
+    --et-friend-directory $ARTUS_FRIENDS_ET \
+    --em-friend-directory $ARTUS_FRIENDS_EM \
+    --mt-friend-directory $ARTUS_FRIENDS_MT \
+    --tt-friend-directory $ARTUS_FRIENDS_TT \
+    --fake-factor-friend-directory $ARTUS_FRIENDS_FAKE_FACTOR
+
+# Normalize fake-factor shapes to nominal
+logandrun python fake-factor-application/normalize_shifts.py output/shapes/${ERA}-${TAG}-${CHANNELS}-shapes.root
