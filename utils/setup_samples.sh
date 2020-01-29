e5741150c27c1fd13b24cf99b07c939450e8a07e
mova <mova@users.noreply.github.com>
2020-01-29 13:56:12 +0100

    fix NNScore friend paths

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -12,21 +12,21 @@ KAPPA_DATABASE=datasets/datasets.json
 # Samples Run2016
 basedir="/ceph/htautau/deeptau"
 ARTUS_OUTPUTS_2016="$basedir/2016/ntuples/"
-NNScore_Friends_2016="$basedir/2016/friends/NNScore/"
+NNScore_Friends_2016="$basedir/2016/friends/NNScore/emb_ff_stage1_fix/"
 SVFit_Friends_2016="$basedir/2016/friends/SVFit/"
 MELA_Friends_2016="$basedir/2016/friends/MELA/"
 FF_Friends_2016="$basedir/2016/friends/FakeFactors/"
 
 # Samples Run2017
 ARTUS_OUTPUTS_2017="$basedir/2017/ntuples/"
-NNScore_Friends_2017="$basedir/2017/friends/NNScore/"
+NNScore_Friends_2017="$basedir/2017/friends/NNScore/emb_ff_stage1_fix/"
 SVFit_Friends_2017="$basedir/2017/friends/SVFit/"
 MELA_Friends_2017="$basedir/2017/friends/MELA/"
 FF_Friends_2017="$basedir/2017/friends/FakeFactors/"
 
 # Samples Run2018
 ARTUS_OUTPUTS_2018="$basedir/2018/ntuples/"
-NNScore_Friends_2018="$basedir/2018/friends/NNScore/"
+NNScore_Friends_2018="$basedir/2018/friends/NNScore/emb_ff_stage1_fix/"
 SVFit_Friends_2018="$basedir/2018/friends/SVFit/"
 MELA_Friends_2018="$basedir/2018/friends/MELA/"
 FF_Friends_2018="$basedir/2018/friends/FakeFactors/"
