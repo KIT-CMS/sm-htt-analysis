82e6ef60bfc918f1cb42d0123c3d353c09911011
Janek Bechtel <janek.bechtel@student.kit.edu>
2020-02-17 08:49:49 +0100

    Adapt hp plotting to new output directory

--- plot_nominal.sh
+++ plot_nominal.sh
@@ -4,10 +4,10 @@ VARIABLE=$3
 
 ./shapes/produce_shapes_variables_nominal.sh $ERA $CHANNEL $VARIABLE 
 
-python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL --emb --ff &
-python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL --emb  &
-python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL --ff  &
-python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}--shapes.root -c $CHANNEL  &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}_${CHANNEL}_${VARIABLE}/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}-${CHANNEL}-shapes.root -c $CHANNEL --emb --ff &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}_${CHANNEL}_${VARIABLE}/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}-${CHANNEL}-shapes.root -c $CHANNEL --emb  &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}_${CHANNEL}_${VARIABLE}/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}-${CHANNEL}-shapes.root -c $CHANNEL --ff  &
+python plot_nominal.py  -v $VARIABLE --era Run${ERA}  --shapes output/shapes/${ERA}_${CHANNEL}_${VARIABLE}/${ERA}-${ERA}_${CHANNEL}_${VARIABLE}-${CHANNEL}-shapes.root -c $CHANNEL  &
 
 wait
 
