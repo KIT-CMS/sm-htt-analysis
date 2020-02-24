1f481d6ddb6165deb3d352088aa639eb142ab547
Maximilian Burkart <maxiburkart@gmail.com>
2020-02-24 15:43:38 +0100

    Update blinding script to match output structure of shape production

--- shapes/apply_blinding.sh
+++ shapes/apply_blinding.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ERA=$1
+CHANNEL=$2
+TAG=$3
 
 source utils/setup_cvmfs_sft.sh
 
-python shapes/apply_blinding.py ${ERA}_shapes.root \
+python shapes/apply_blinding.py output/shapes/${TAG}/${ERA}-${TAG}-${CHANNEL}-shapes.root \
     --threshold 0.5 --uncertainty 0.09 \
     --signal-processes ggH qqH VH \
     --exclude-categories _ss _B _sumW _sumEWKZ _FF
