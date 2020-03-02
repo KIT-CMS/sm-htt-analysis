cc79b50c298f77b7ede56d5dec46f296a6a6052d
Maximilian Burkart <maxiburkart@gmail.com>
2020-03-02 15:01:01 +0100

    [GOF] Fix missing output variable in jdl creation.

--- gof/create_jdl.sh
+++ gof/create_jdl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ERA=$1
+OUTPUT=$2
 
 source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
@@ -8,5 +9,3 @@ source utils/setup_python.sh
 source utils/bashFunctionCollection.sh
 
 logandrun python gof/create_jdl.py $ERA $OUTPUT gof/${ERA}_binning.yaml gof/variables.yaml
-
-# Change line in 
