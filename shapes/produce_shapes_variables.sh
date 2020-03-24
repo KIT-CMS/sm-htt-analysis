b526fb89de724a45034528bd8c9994a7ab06a107
Maximilian Burkart <maxiburkart@gmail.com>
2020-03-24 21:06:38 +0100

    Update variable production for control_shapes.

--- shapes/produce_shapes_variables.sh
+++ shapes/produce_shapes_variables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-BINNING=shapes/binning.yaml
 ERA=$1
 CHANNELS=$2
 VARIABLE=$3
+[[ ! -z $4 ]] && NUM_THREADS=$4 || NUM_THREADS=8
+BINNING=shapes/binning.yaml
 
 source utils/setup_cvmfs_sft.sh
 source utils/setup_python.sh
@@ -14,20 +15,31 @@ source utils/setup_samples.sh $ERA
 #./shapes/calculate_binning.sh et
 #./shapes/calculate_binning.sh mt
 #./shapes/calculate_binning.sh tt
+if [[ "$CHANNELS" =~ "em" ]]
+then
+    PROCESSES=data_obs,EMB,ZTT,ZL,TTL,TTT,VVL,VVT,W,WH125,ZH125,ttH125,ggHWW125,qqHWW125,ggH125,qqH125,WHWW125,ZHWW125,QCD
+else
+    PROCESSES=data_obs,EMB,ZTT,ZL,TTL,TTT,VVL,VVT,WH125,ZH125,ttH125,ggHWW125,qqHWW125,ggH125,qqH125,WHWW125,ZHWW125,FAKES
+fi
 
 # Produce shapes
-
 python shapes/produce_shapes.py \
     --directory $ARTUS_OUTPUTS \
-    --et-friend-directory $ARTUS_FRIENDS_ET \
-    --em-friend-directory $ARTUS_FRIENDS_EM \
-    --mt-friend-directory $ARTUS_FRIENDS_MT \
-    --tt-friend-directory $ARTUS_FRIENDS_TT \
-    --fake-factor-friend-directory $ARTUS_FRIENDS_FAKE_FACTOR \
     --datasets $KAPPA_DATABASE \
     --binning $BINNING \
     --channels $CHANNELS \
     --gof-variable $VARIABLE \
+    --processes $PROCESSES \
+    --categories control \
+    --em-friend-directory $ARTUS_FRIENDS_EM \
+    --et-friend-directory $ARTUS_FRIENDS_ET \
+    --mt-friend-directory $ARTUS_FRIENDS_MT \
+    --tt-friend-directory $ARTUS_FRIENDS_TT \
+    --fake-factor-friend-directory $ARTUS_FRIENDS_FAKE_FACTOR \
     --era $ERA \
-    --tag ${ERA}_${CHANNELS}_${VARIABLE} \
-    --num-threads 38
+    --tag ${VARIABLE}-control \
+    --num-threads $NUM_THREADS
+
+mv output/shapes/${VARIABLE}-control/${ERA}-${VARIABLE}-control-${CHANNELS}-${PROCESSES}-control-shapes.root output/shapes/${VARIABLE}-control/${ERA}-${VARIABLE}-control-${CHANNELS}-shapes.root
+# Normalize fake-factor shapes to nominal
+python fake-factor-application/normalize_shifts.py output/shapes/${VARIABLE}-control/${ERA}-${VARIABLE}-control-${CHANNELS}-shapes.root
