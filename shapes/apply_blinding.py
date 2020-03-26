2f6301ab4d4753d941e9f8173e8a42c5b02d6d98
Sebastian Brommer <sebastian.brommer@kit.edu>
2020-03-26 13:47:41 +0100

    update blinding script

--- shapes/apply_blinding.py
+++ shapes/apply_blinding.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import ROOT
-ROOT.PyConfig.IgnoreCommandLineOptions = True  # disable ROOT internal argument parser
-
 import argparse
 import os
 import numpy as np
+from array import array
 
 import logging
 import re
 logger = logging.getLogger("")
+
+ROOT.PyConfig.IgnoreCommandLineOptions = True  # disable ROOT internal argument parser
+ROOT.gErrorIgnoreLevel = ROOT.kError
 
 
 def setup_logging(output_file, level=logging.DEBUG):
@@ -43,27 +45,39 @@ def parse_arguments():
         required=True,
         type=float,
         help="Uncertainty of background yield in term (e*b)**2.")
-    parser.add_argument(
-        "--signal-processes",
-        nargs="+",
-        required=True,
-        help="Substrings to be present in signal categories.")
+    parser.add_argument("--signal-processes",
+                        nargs="+",
+                        required=True,
+                        help="Substrings to be present in signal categories.")
     parser.add_argument(
         "--exclude-categories",
         nargs="+",
         required=True,
         help="Substrings to be present in excluded categories.")
+    parser.add_argument("--exclude-processes",
+                        nargs="+",
+                        default=["data"],
+                        help="Substrings to be present in excluded processes.")
     parser.add_argument(
-        "--exclude-processes",
+        "--global_blinding",
+        action="store_true",
+        help=
+        "If set, the treshold is applied as a blinding cut on the NN Score in all signal categories"
+    )
+    parser.add_argument(
+        "--signal-categories",
         nargs="+",
-        default=["data"],
-        help="Substrings to be present in excluded processes.")
+        required=False,
+        help=
+        "List of all signal categories to be blinded, only used when applying global_blinding"
+    )
 
     return parser.parse_args()
 
 
 def main(args):
     # Open file with shapes
+    logger.debug("Shapesfile: {}".format(args.shapes))
     if not os.path.exists(args.shapes):
         logger.fatal("File %s does not exist.", args.shapes)
         raise Exception
@@ -126,7 +140,8 @@ def main(args):
         for category in shapes[channel]:
             for process in shapes[channel][category]["processes"]:
                 for substring in args.signal_processes:
-                    if substring in process and not process in signal_processes[channel]:
+                    if substring in process and not process in signal_processes[
+                            channel]:
                         signal_processes[channel].append(process)
         logger.debug("Identified signal processes for channel %s: %s", channel,
                      signal_processes[channel])
@@ -142,8 +157,8 @@ def main(args):
                     shapes[channel][category]["processes"]):
                 if process in signal_processes[channel]:
                     if signals == None:
-                        signals = file_.Get(shapes[channel][category][
-                            "histograms"][i]).Clone()
+                        signals = file_.Get(shapes[channel][category]
+                                            ["histograms"][i]).Clone()
                     else:
                         signals.Add(
                             file_.Get(
@@ -151,31 +166,51 @@ def main(args):
                             1.0)
                 else:
                     if backgrounds == None:
-                        backgrounds = file_.Get(shapes[channel][category][
-                            "histograms"][i]).Clone()
+                        backgrounds = file_.Get(shapes[channel][category]
+                                                ["histograms"][i]).Clone()
                     else:
                         backgrounds.Add(
                             file_.Get(
                                 shapes[channel][category]["histograms"][i]),
                             1.0)
-
-            # Check for bins above threshold
-            bins_contaminated = []
-            for i in range(1, signals.GetNbinsX() + 1):
-                s = signals.GetBinContent(i)
-                b = backgrounds.GetBinContent(i)
-                e = args.uncertainty
-                if s + b == 0:
+            # remove all bins in all signal categories above given threshold
+            if args.global_blinding is not None:
+                bins_contaminated = []
+                logger.debug("Checking {}".format(category[3::]))
+                if category[3::] not in args.signal_categories:
+                    logger.debug(
+                        "this is not a signal category, continuing....")
                     continue
-                if s / np.sqrt(s + b + (e * b)**2) > args.threshold:
-                    bins_contaminated.append(i)
-            logger.debug("Bins with signal contamination in category %s: %s",
-                         category, bins_contaminated)
+                else:
+                    binning = []
 
-            # Set signal contaminated bins to zero for all shapes of this channel and category
-            if not len(bins_contaminated) == 0:
-                for key in file_.GetListOfKeys():
+                    for i in range(1, signals.GetNbinsX() + 1):
+                        binning.append(signals.GetBinLowEdge(i))
+                        if signals.GetBinLowEdge(i) >= args.threshold:
+                            bins_contaminated.append(i)
+                    if "xxh" in category:  # special care for 2D ggh/qqh category
+                        logger.debug("Special treatment for 2D category")
+                        bins_contaminated = range(11, 26)
+                    logger.debug("Binning for {}: {}".format(
+                        category, binning))
+                    logger.debug(
+                        "Bins with signal contamination in category {}: {}".
+                        format(category, bins_contaminated))
+                good_bins = range(1, bins_contaminated[1])
+                logger.debug("Keeping Bins in category {}: {}".format(
+                    category, good_bins))
+                nbinsx = len(good_bins) - 1
+                # remove contaminated bins from the histograms --> write new histogram with limited bins
+                logger.debug("Updating histograms")
+                filesize = len(file_.GetListOfKeys())
+                # create a static list, otherwise, the loop runs forever
+                filekeys = [key for key in file_.GetListOfKeys()]
+                for s, key in enumerate(filekeys):
+                    if s % 10000 == 0:
+                        logger.debug("Progress: {} / {}".format(s, filesize))
                     name = key.GetName()
+                    if name == "output_tree":
+                        continue
                     split = [x for x in name.split('#') if x != '']
                     channel_ = split[0]
                     category_ = split[1]
@@ -183,11 +218,52 @@ def main(args):
                         continue
                     if not category_ == category:
                         continue
-                    hist = file_.Get(name)
-                    for i in bins_contaminated:
-                        hist.SetBinContent(i, 0.0)
-                        hist.SetBinError(i, 0.0)
-                    hist.Write()
+                    oldhist = file_.Get(name)
+                    bin_borders = [oldhist.GetBinLowEdge(x) for x in good_bins]
+                    # bin_borders.append(oldhist.GetBinLowEdge(good_bins[-1]+1))
+                    newhist = ROOT.TH1D(name, name, nbinsx,
+                                        array("d", bin_borders))
+                    for i in good_bins:
+                        newhist.SetBinContent(i, oldhist.GetBinContent(i))
+                        newhist.SetBinError(i, oldhist.GetBinError(i))
+                    if "xxh" in category:  # special care for ggh/qqh 2D category
+                        for i in [5, 6, 7]:
+                            newhist.SetBinContent(i, 0.0)
+                            newhist.SetBinError(i, 0.0)
+                    newhist.Write("", ROOT.TObject.kOverwrite)
+            else:
+                # Check for bins above threshold
+                bins_contaminated = []
+                for i in range(1, signals.GetNbinsX() + 1):
+                    s = signals.GetBinContent(i)
+                    b = backgrounds.GetBinContent(i)
+                    e = args.uncertainty
+                    if s + b == 0:
+                        continue
+                    if s / np.sqrt(s + b + (e * b)**2) > args.threshold:
+                        bins_contaminated.append(i)
+                logger.debug(
+                    "Bins with signal contamination in category %s: %s",
+                    category, bins_contaminated)
+                # Set signal contaminated bins to zero for all shapes of this channel and category
+                if not len(bins_contaminated) == 0:
+
+                    for key in file_.GetListOfKeys():
+                        name = key.GetName()
+                        if name == "output_tree":
+                            continue
+                        split = [x for x in name.split('#') if x != '']
+                        channel_ = split[0]
+                        category_ = split[1]
+                        if not channel_ == channel:
+                            continue
+                        if not category_ == category:
+                            continue
+                        hist = file_.Get(name)
+                        for i in bins_contaminated:
+                            hist.SetBinContent(i, 0.0)
+                            hist.SetBinError(i, 0.0)
+                        hist.Write()
 
 
 if __name__ == "__main__":
