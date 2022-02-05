90c435bc69632af72667cf78b6a867c48ed1df86
ralfschmieder <ralf.schmieder@student.kit.edu>
2022-02-05 11:19:59 +0100

    integrated a parametrized training

--- ml/combine_configs.sh
+++ ml/combine_configs.sh
@@ -1,7 +1,7 @@
 ERA=$1
 CHANNEL=$2
-TAG=$3
+ANALYSIS_NAME=$3
 echo $TAG
 source utils/setup_cvmfs_sft.sh
-python ml/create_combined_config.py  --tag $TAG --channel $CHANNEL --output_dir output/ml/${ERA}_${CHANNEL}_${TAG}
+python ml/create_combined_config.py  --tag $ANALYSIS_NAME --channel $CHANNEL --output_dir output/ml/${ANALYSIS_NAME}/${ERA}_${CHANNEL}
 
